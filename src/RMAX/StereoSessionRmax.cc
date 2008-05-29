f1055a580282c99ba54f7d4b62501e750946cfe1
Michael Broxton <michael.broxton@nasa.gov>
2008-05-29 01:13:44 +0000

    Forgot to check in the RMAX stereo session.

---
+++ src/RMAX/StereoSessionRmax.cc
@@ -0,0 +1,17 @@
+#include <boost/shared_ptr.hpp>
+
+#include "RMAX/StereoSessionRmax.h"
+#include "RMAX/RMAX.h"
+
+using namespace vw;
+using namespace vw::camera;
+
+boost::shared_ptr<vw::camera::CameraModel> StereoSessionRmax::camera_model(std::string image_file, 
+                                                                           std::string camera_file) {
+
+  ImageInfo info; 
+  read_image_info( image_file, info );
+  CAHVORModel* cahvor = new CAHVORModel;
+  *cahvor = rmax_image_camera_model(info);
+  return boost::shared_ptr<camera::CameraModel>(cahvor);
+}
---
+++ src/RMAX/StereoSessionRmax.h
@@ -0,0 +1,19 @@
+#ifndef __RMAX_STEREO_SESSION_H__
+#define __RMAX_STEREO_SESSION_H__
+
+#include "StereoSessionPinhole.h"
+
+class StereoSessionRmax: public StereoSessionPinhole {
+  
+public:
+
+  virtual ~StereoSessionRmax() {}
+
+  // Correct lens distortion and epipolar-rectify the images
+  virtual boost::shared_ptr<vw::camera::CameraModel> camera_model(std::string image_file, 
+                                                                  std::string camera_file = "");
+
+  static StereoSession* construct() { return new StereoSessionRmax; }
+};
+
+#endif // __RMAX_STEREO_SESSION_H__
