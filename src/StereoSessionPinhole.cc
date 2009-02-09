4b8cc8d2d67fed1fa10e5ed2c211ea3f2970e71a
Michael Broxton <michael.broxton@nasa.gov>
2009-02-09 22:53:33 +0000

    Taking a crack at fixing Zach\'s interest point alignment problem that results in bubbling of the 3D mesh.

--- src/StereoSessionPinhole.cc
+++ src/StereoSessionPinhole.cc
@@ -206,81 +206,74 @@ vw::math::Matrix<double> StereoSessionPinhole::determine_keypoint_alignment( std
 
 boost::shared_ptr<vw::camera::CameraModel> StereoSessionPinhole::camera_model(std::string image_file, 
                                                                               std::string camera_file) {
-  // Keypoint alignment
-  if ( stereo_settings().keypoint_alignment ) {
-    if (boost::ends_with(boost::to_lower_copy(camera_file),".cahvor") ||
-	boost::ends_with(boost::to_lower_copy(camera_file),".cmod") ) {
-      DiskImageView<PixelGray<float> > image(image_file);
-      CAHVORModel camera(camera_file);
-      boost::shared_ptr<CAHVModel> output( new CAHVModel );
-      *output = linearize_camera( camera,
-				  image.cols(), image.rows(),
-				  image.cols(), image.rows() );
-      return output;
-    } else if ( boost::ends_with(boost::to_lower_copy(camera_file),".cahv") ||
-		boost::ends_with(boost::to_lower_copy(camera_file),".pin") ) {
-      boost::shared_ptr<CAHVModel> output( new CAHVModel( camera_file ) );
-      return output;
-    } else if ( boost::ends_with(boost::to_lower_copy(camera_file),"tsai") ) {
-      PinholeModel camera(camera_file);
-      boost::shared_ptr<CAHVModel> output( new CAHVModel );
-      *output = linearize_camera( camera );
-      return output;
+  // Epipolar Alignment
+  if ( stereo_settings().epipolar_alignment ) {
+    // Load the image
+    DiskImageView<PixelGray<float> > left_image(m_left_image_file);
+    DiskImageView<PixelGray<float> > right_image(m_right_image_file);
+
+    bool is_left_camera = true;
+    if (camera_file == m_left_camera_file) 
+      is_left_camera = true;
+    else if (camera_file == m_right_camera_file) 
+      is_left_camera = false;
+    else 
+      (ArgumentErr() << "StereoSessionPinhole: supplied camera model filename does not match the name supplied in the constructor.");
+
+    // Return the appropriate camera model object
+    CAHVModel left_cahv, right_cahv;
+    if (boost::ends_with(boost::to_lower_copy(m_left_camera_file), ".cahvor")  || 
+        boost::ends_with(boost::to_lower_copy(m_left_camera_file), ".cmod") ) {
+      CAHVORModel left_cahvor(m_left_camera_file);
+      CAHVORModel right_cahvor(m_right_camera_file);
+      left_cahv = linearize_camera(left_cahvor, 
+                                   left_image.cols(), left_image.rows(),
+                                   left_image.cols(), left_image.rows());
+      right_cahv = linearize_camera(right_cahvor, 
+                                    right_image.cols(), right_image.rows(),
+                                    right_image.cols(), right_image.rows());
+    } else if ( boost::ends_with(boost::to_lower_copy(m_left_camera_file), ".cahv") ||
+                boost::ends_with(boost::to_lower_copy(m_left_camera_file), ".pin" )) {
+      left_cahv = CAHVModel(m_left_camera_file);
+      right_cahv = CAHVModel(m_right_camera_file);
+
+    } else if ( boost::ends_with(boost::to_lower_copy(m_left_camera_file), ".tsai") ) {
+      PinholeModel left_pin(m_left_camera_file);
+      PinholeModel right_pin(m_right_camera_file);
+      left_cahv = linearize_camera(left_pin);
+      right_cahv = linearize_camera(right_pin);
+
     } else {
       vw_throw(ArgumentErr() << "PinholeStereoSession: unsupported cameara file type.\n");
     }
+
+    // Create epipolar recitified camera views
+    boost::shared_ptr<CAHVModel> epipolar_left_cahv(new CAHVModel);
+    boost::shared_ptr<CAHVModel> epipolar_right_cahv(new CAHVModel);
+    epipolar(left_cahv, right_cahv, *epipolar_left_cahv, *epipolar_right_cahv);
+
+    if (is_left_camera)
+      return epipolar_left_cahv;
+    else 
+      return epipolar_right_cahv;
   }
 
-  // Epipolar Alignment
-
-  // Load the image
-  DiskImageView<PixelGray<float> > left_image(m_left_image_file);
-  DiskImageView<PixelGray<float> > right_image(m_right_image_file);
-
-  bool is_left_camera = true;
-  if (camera_file == m_left_camera_file) 
-    is_left_camera = true;
-  else if (camera_file == m_right_camera_file) 
-    is_left_camera = false;
-  else 
-    (ArgumentErr() << "StereoSessionPinhole: supplied camera model filename does not match the name supplied in the constructor.");
-
-  // Return the appropriate camera model object
-  CAHVModel left_cahv, right_cahv;
-  if (boost::ends_with(boost::to_lower_copy(m_left_camera_file), ".cahvor")  || 
-      boost::ends_with(boost::to_lower_copy(m_left_camera_file), ".cmod") ) {
-    CAHVORModel left_cahvor(m_left_camera_file);
-    CAHVORModel right_cahvor(m_right_camera_file);
-    left_cahv = linearize_camera(left_cahvor, 
-                                 left_image.cols(), left_image.rows(),
-                                 left_image.cols(), left_image.rows());
-    right_cahv = linearize_camera(right_cahvor, 
-                                  right_image.cols(), right_image.rows(),
-                                  right_image.cols(), right_image.rows());
-  } else if ( boost::ends_with(boost::to_lower_copy(m_left_camera_file), ".cahv") ||
-              boost::ends_with(boost::to_lower_copy(m_left_camera_file), ".pin" )) {
-    left_cahv = CAHVModel(m_left_camera_file);
-    right_cahv = CAHVModel(m_right_camera_file);
-
-  } else if ( boost::ends_with(boost::to_lower_copy(m_left_camera_file), ".tsai") ) {
-    PinholeModel left_pin(m_left_camera_file);
-    PinholeModel right_pin(m_right_camera_file);
-    left_cahv = linearize_camera(left_pin);
-    right_cahv = linearize_camera(right_pin);
-
+  // Keypoint alignment
+  else if ( stereo_settings().keypoint_alignment ) {
+    if (boost::ends_with(boost::to_lower_copy(camera_file),".cahvor") ||
+	boost::ends_with(boost::to_lower_copy(camera_file),".cmod") ) {
+      return boost::shared_ptr<vw::camera::CameraModel>( new CAHVORModel(camera_file) );
+    } else if ( boost::ends_with(boost::to_lower_copy(camera_file),".cahv") ||
+		boost::ends_with(boost::to_lower_copy(camera_file),".pin") ) {
+      return boost::shared_ptr<vw::camera::CameraModel>( new CAHVModel(camera_file) );
+    } else if ( boost::ends_with(boost::to_lower_copy(camera_file),"tsai") ) {
+      return boost::shared_ptr<vw::camera::CameraModel> ( new PinholeModel(camera_file) );
+    } else {
+      vw_throw(ArgumentErr() << "PinholeStereoSession: unsupported cameara file type.\n");
+    }
   } else {
-    vw_throw(ArgumentErr() << "PinholeStereoSession: unsupported cameara file type.\n");
+    vw_throw(ArgumentErr() << "PinholeStereoSession: no alignment method was selected in your stereo.default file..\n");
   }
-
-  // Create epipolar recitified camera views
-  boost::shared_ptr<CAHVModel> epipolar_left_cahv(new CAHVModel);
-  boost::shared_ptr<CAHVModel> epipolar_right_cahv(new CAHVModel);
-  epipolar(left_cahv, right_cahv, *epipolar_left_cahv, *epipolar_right_cahv);
-
-  if (is_left_camera)
-    return epipolar_left_cahv;
-  else 
-    return epipolar_right_cahv;
 }
 
 void StereoSessionPinhole::pre_preprocessing_hook(std::string const& input_file1, std::string const& input_file2,
@@ -339,7 +332,7 @@ void StereoSessionPinhole::pre_preprocessing_hook(std::string const& input_file1
 		     left_disk_image.cols(), left_disk_image.rows());
 
   } else {
-    vw_throw(ArgumentErr() << "PinholeStereoSession: unselected alignment option.\n");
+    vw_throw(ArgumentErr() << "PinholeStereoSession: no alignment method was selected in your stereo.default file..\n");
   }
 
   output_file1 = m_out_prefix + "-L.tif";
@@ -370,15 +363,13 @@ void StereoSessionPinhole::pre_pointcloud_hook(std::string const& input_file,
       exit(1);
     }
 
-    result = stereo::disparity::transform_disparities( disparity_map,
-						       HomographyTransform(align_matrix));
+    result = stereo::disparity::transform_disparities( disparity_map,HomographyTransform(align_matrix));
 
     // Remove pixels that are outside the bounds of the second image
     DiskImageView<PixelGray<float> > right_disk_image( m_right_image_file );
     result = stereo::disparity::remove_invalid_pixels( result, right_disk_image.cols(), right_disk_image.rows()); 
 
-    write_image(output_file, result,
-		TerminalProgressCallback(ErrorMessage, "\t    Saving: ") );
+    write_image(output_file, result, TerminalProgressCallback(ErrorMessage, "\t    Saving: ") );
 
   } else {
     vw_throw(ArgumentErr() << "PinholeStereoSession: unselected alignment option.\n");
