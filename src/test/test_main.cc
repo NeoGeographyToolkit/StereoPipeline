9a18aafd8a989938fbd223e7d062918e5aed9aa8
Zack Moratto <zachary.m.moratto@nasa.gov>
2010-01-28 07:39:04 +0000

    Added gtest to ASP
    In the future there will be tests.

---
+++ src/test/test_main.cc
@@ -0,0 +1,9 @@
+#include <gtest/gtest.h>
+#include <vw/Core/Settings.h>
+
+int main(int argc, char **argv) {
+  // Disable the user's config file
+  vw::vw_settings().set_rc_filename("");
+  testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
