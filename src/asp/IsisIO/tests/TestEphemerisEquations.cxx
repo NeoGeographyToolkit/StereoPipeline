08e62125ea07260a994139033a3ab3d32c34e56d
Zack Moratto <zachary.m.moratto@nasa.gov>
2010-01-30 09:28:15 +0000

    OMG, forgot to add the GTest files
    Yep .. I did that.

---
+++ src/asp/IsisIO/tests/TestEphemerisEquations.cxx
@@ -0,0 +1,116 @@
+// __BEGIN_LICENSE__
+// Copyright (C) 2006-2009 United States Government as represented by
+// the Administrator of the National Aeronautics and Space Administration.
+// All Rights Reserved.
+// __END_LICENSE__
+
+#include <gtest/gtest.h>
+
+#include <asp/IsisIO/PolyEquation.h>
+#include <asp/IsisIO/RPNEquation.h>
+
+using namespace vw;
+using namespace vw::camera;
+
+double DELTA = 1e-6;
+
+TEST(EphemerisEquations, polynomial_equal_order) {
+  PolyEquation poly(0);
+  EXPECT_EQ( poly.size(), 3 );
+  EXPECT_EQ( poly.type(), "PolyEquation" );
+  poly[0] = 10;
+  poly[1] = 1;
+  poly[2] = 20;
+  EXPECT_EQ( poly[0], 10 );
+  EXPECT_EQ( poly[1], 1 );
+  EXPECT_EQ( poly[2], 20 );
+
+  Vector3 test = poly(20);
+  EXPECT_EQ( 10, test[0] );
+  EXPECT_EQ( 1,  test[1] );
+  EXPECT_EQ( 20, test[2] );
+
+  poly[1] = -50;
+  test = poly(-100);
+  EXPECT_EQ( 10,  test[0] );
+  EXPECT_EQ( -50, test[1] );
+  EXPECT_EQ( 20,  test[2] );
+
+  PolyEquation poly2(1);
+  EXPECT_EQ( poly2.size(), 6 );
+  poly2[0] = 10; poly2[1] = 0.5;
+  poly2[2] = 5; poly2[3] = -0.5;
+  poly2[4] = 0; poly2[5] = 6;
+
+  test = poly2(20);
+  EXPECT_EQ( 20,  test[0] );
+  EXPECT_EQ( -5,  test[1] );
+  EXPECT_EQ( 120, test[2] );
+}
+
+TEST(EphemerisEquations, polynomial_variable_order) {
+  PolyEquation poly(0,2,1);
+  EXPECT_EQ( 6, poly.size() );
+  EXPECT_EQ( poly.type(), "PolyEquation" );
+  poly[0] = 11;
+  poly[1] = -5; poly[2] = 0.6; poly[3] = .1;
+  poly[4] = -4; poly[5] = 2.5;
+  EXPECT_EQ( -5, poly[1] );
+
+  Vector3 test = poly(-1.5);
+  EXPECT_NEAR( 11, test[0], DELTA );
+  EXPECT_NEAR( -5.675, test[1], DELTA );
+  EXPECT_NEAR( -7.75, test[2], DELTA );
+}
+
+TEST(EphemerisEquations, polynomial_defined_const) {
+  Vector<double> x_coeff(1);
+  x_coeff[0] = 11;
+  Vector<double> y_coeff(3);
+  y_coeff[0] = -5;
+  y_coeff[1] = 0.6;
+  y_coeff[2] = .1;
+  Vector<double> z_coeff(2);
+  z_coeff[0] = -4;
+  z_coeff[1] = 2.5;
+  PolyEquation poly(x_coeff,
+                    y_coeff,
+                    z_coeff);
+  EXPECT_EQ( 6, poly.size() );
+  EXPECT_EQ( poly.type(), "PolyEquation" );
+  EXPECT_EQ( -5, poly[1] );
+
+  Vector3 test = poly(-1.5);
+  EXPECT_NEAR( 11, test[0], DELTA );
+  EXPECT_NEAR( -5.675, test[1], DELTA );
+  EXPECT_NEAR( -7.75, test[2], DELTA );
+}
+
+TEST(EphemerisEquations, reversepolish) {
+  std::string x_eq("3 t t * * 1 +");
+  std::string y_eq("t sin 4 * t +");
+  std::string z_eq("t t 2 * * 5 t / - abs");
+  RPNEquation rpn( x_eq, y_eq, z_eq );
+  EXPECT_EQ( 5, rpn.size() );
+  EXPECT_EQ( "RPNEquation", rpn.type() );
+
+  Vector3 test = rpn(-1.5);
+  EXPECT_NEAR( 7.75, test[0], DELTA );
+  EXPECT_NEAR( -5.48997994641622, test[1], DELTA );
+  EXPECT_NEAR( 7.8333333333, test[2], DELTA );
+
+  rpn[0] = 22;
+  rpn[2] = 9;
+  rpn[3] = 8;
+  test = rpn(-1.5);
+  EXPECT_NEAR( 50.5, test[0], DELTA );
+  EXPECT_NEAR( -10.4774548794365, test[1], DELTA );
+  EXPECT_NEAR( 21.333333333, test[2], DELTA );
+
+  rpn.set_time_offset( -20 );
+  EXPECT_EQ( -20, rpn.get_time_offset() );
+  test = rpn(-1.5); // like eval with 18.5
+  EXPECT_NEAR( 7530.5, test[0], DELTA );
+  EXPECT_NEAR( 15.4176744337735, test[1], DELTA );
+  EXPECT_NEAR( 2737.72972972973, test[2], DELTA );
+}
---
+++ src/asp/IsisIO/tests/TestIsisAdjustCameraModel.cxx
@@ -0,0 +1,179 @@
+// __BEGIN_LICENSE__
+// Copyright (C) 2006-2009 United States Government as represented by
+// the Administrator of the National Aeronautics and Space Administration.
+// All Rights Reserved.
+// __END_LICENSE__
+
+#include <gtest/gtest.h>
+
+#include <asp/IsisIO/IsisAdjustCameraModel.h>
+
+using namespace vw;
+using namespace vw::camera;
+
+Vector2 generate_random( int const& xsize,
+                         int const& ysize ) {
+  Vector2 pixel;
+  pixel[0] = rand() % ( 10 * xsize - 10 ) + 10;
+  pixel[0] /= 10;
+  pixel[1] = rand() % ( 10 * ysize - 10 ) + 10;
+  pixel[1] /= 10;
+  return pixel;
+}
+
+TEST(IsisAdjustCameraModel, NoFunctions) {
+  std::vector<std::string> files;
+  files.push_back("E1701676.crop.cub");
+  files.push_back("5165r.cub");
+  srand( time(NULL) );
+  for ( uint j = 0; j < files.size(); j++ ) {
+    boost::shared_ptr<BaseEquation> blank( new PolyEquation(0) );
+    IsisAdjustCameraModel cam( files[j], blank, blank );
+
+    std::cout << "File: " << files[j] << "\n";
+    std::cout << "-------------------------------------\n";
+
+    for ( uint i = 0; i < 100; i++ ) {
+      Vector2 pixel = generate_random( cam.samples(),
+                                       cam.lines() );
+      Vector3 point = cam.pixel_to_vector( pixel );
+      for ( uint k = 0; k < 5; k++ ) {
+        // Apply noise to make sure we are not using stored values
+        Vector2 noise = generate_random( cam.samples(),
+                                         cam.lines() );
+        Vector3 temp = cam.pixel_to_vector( noise );
+      }
+      point *= 100000; // 100 km below
+      point += cam.camera_center( pixel );
+      for ( uint k = 0; k < 5; k++ ) {
+        // Apply noise to make sure we are not using stored values
+        Vector2 noise = generate_random( cam.samples(),
+                                         cam.lines() );
+        Vector3 temp = cam.camera_center( noise );
+      }
+
+      Vector2 rpixel = cam.point_to_pixel( point );
+      EXPECT_NEAR( pixel[0], rpixel[0], 0.001 );
+      EXPECT_NEAR( pixel[1], rpixel[1], 0.001 );
+    }
+  }
+}
+
+TEST(IsisAdjustCameraModel, PolyFunctions) {
+  std::vector<std::string> files;
+  files.push_back("E1701676.crop.cub");
+  files.push_back("5165r.cub");
+  srand( time(NULL) );
+  for ( uint j = 0; j < files.size(); j++ ) {
+    boost::shared_ptr<BaseEquation> position( new PolyEquation(1) );
+    (*position)[0] = 1000;
+    (*position)[1] = 10;
+    (*position)[2] = 2000;
+    (*position)[3] = -10;
+    (*position)[4] = -11000;
+    (*position)[5] = 5;
+    boost::shared_ptr<BaseEquation> pose( new PolyEquation(0) );
+    (*position)[0] = .07;
+    (*position)[1] = -.1;
+    (*position)[2] = .02;
+    IsisAdjustCameraModel cam( files[j], position, pose );
+    std::cout << "File: " << files[j] << "\n";
+    std::cout << "---------------------------------------\n";
+
+    for ( uint i = 0; i < 100; i++ ) {
+      Vector2 pixel = generate_random( cam.samples(),
+                                       cam.lines() );
+      Vector3 point = cam.pixel_to_vector( pixel );
+      for ( uint k = 0; k < 5; k++ ) {
+        // Apply noise
+        Vector2 noise = generate_random( cam.samples(),
+                                         cam.lines() );
+        Vector3 temp = cam.pixel_to_vector( noise );
+      }
+      point *= 100000; // 100 km
+      point += cam.camera_center( pixel );
+      for ( uint k = 0; k < 5; k++ ) {
+        // Apply noise
+        Vector2 noise = generate_random( cam.samples(),
+                                         cam.lines() );
+        Vector3 temp = cam.camera_center( noise );
+      }
+      Vector2 rpixel = cam.point_to_pixel( point );
+      EXPECT_NEAR( pixel[0], rpixel[0], 0.001 );
+      EXPECT_NEAR( pixel[1], rpixel[1], 0.001 );
+      (*position)[2] += 1000;
+      Vector3 rpoint = cam.pixel_to_vector( rpixel );
+      for ( uint k = 0; k < 5; k++ ) {
+        // Apply noise
+        Vector2 noise = generate_random( cam.samples(),
+                                         cam.lines() );
+        Vector3 temp = cam.pixel_to_vector( noise );
+      }
+      rpoint *= 100000; // 100 km
+      rpoint += cam.camera_center( rpixel );
+      EXPECT_NEAR( rpoint[0] - point[0], 0, .001 );
+      EXPECT_NEAR( rpoint[1] - point[1], 1000, .001 );
+      EXPECT_NEAR( rpoint[2] - point[2], 0, .001 );
+    }
+  }
+}
+
+TEST(IsisAdjustCameraModel, RPNFunctions) {
+  std::vector<std::string> files;
+  files.push_back("E1701676.crop.cub");
+  files.push_back("5165r.cub");
+  srand( time(NULL) );
+  for ( uint j = 0; j < files.size(); j++ ) {
+    std::string xpos_eq = "t 2 * 100 / 99 +";
+    std::string ypos_eq = "t .8 * 10000 -";
+    std::string zpos_eq = "t 2 * 2000 +";
+    std::string xang_eq = ".05";
+    std::string yang_eq = "-.03 t *";
+    std::string zang_eq = "0";
+    boost::shared_ptr<BaseEquation> position( new RPNEquation( xpos_eq,
+                                                               ypos_eq,
+                                                               zpos_eq ) );
+    boost::shared_ptr<BaseEquation> pose( new RPNEquation( xang_eq,
+                                                           yang_eq,
+                                                           zang_eq ) );
+    IsisAdjustCameraModel cam( files[j], position, pose );
+    std::cout << "File: " << files[j] << "\n";
+    std::cout << "---------------------------------------\n";
+
+    for ( uint i = 0; i < 100; i++ ) {
+      Vector2 pixel = generate_random( cam.samples(),
+                                       cam.lines() );
+      Vector3 point = cam.pixel_to_vector( pixel );
+      for ( uint k = 0; k < 5; k++ ) {
+        // Apply noise
+        Vector2 noise = generate_random( cam.samples(),
+                                         cam.lines() );
+        Vector3 temp = cam.pixel_to_vector( noise );
+      }
+      point *= 80000; // 80 km
+      point += cam.camera_center( pixel );
+      for ( uint k = 0; k < 5; k++ ) {
+        // Apply noise
+        Vector2 noise = generate_random( cam.samples(),
+                                         cam.lines() );
+        Vector3 temp = cam.camera_center( noise );
+      }
+      Vector2 rpixel = cam.point_to_pixel( point );
+      EXPECT_NEAR( pixel[0], rpixel[0], 0.001 );
+      EXPECT_NEAR( pixel[1], rpixel[1], 0.001 );
+      (*position)[2] -= 500;
+      Vector3 rpoint = cam.pixel_to_vector( rpixel );
+      for ( uint k = 0; k < 5; k++ ) {
+        // Apply noise
+        Vector2 noise = generate_random( cam.samples(),
+                                         cam.lines() );
+        Vector3 temp = cam.pixel_to_vector( noise );
+      }
+      rpoint *= 80000; // 80 km
+      rpoint += cam.camera_center( rpixel );
+      EXPECT_NEAR( rpoint[0] - point[0], -500, .001 );
+      EXPECT_NEAR( rpoint[1] - point[1], 0, .001 );
+      EXPECT_NEAR( rpoint[2] - point[2], 0, .001 );
+    }
+  }
+}
