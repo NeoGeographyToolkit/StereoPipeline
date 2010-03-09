e92cba49d6586f6acf05d8e5591c9de90e4e0eac
Zack Moratto <zachary.m.moratto@nasa.gov>
2010-03-09 05:52:22 +0000

    A better demo of how to use MPI for isis_adjust

--- src/asp/MPI/isis_mpi_adjust.cc
+++ src/asp/MPI/isis_mpi_adjust.cc
@@ -4,19 +4,116 @@
 #include <boost/serialization/string.hpp>
 namespace mpi = boost::mpi;
 
+#include <vw/Core.h>
+using namespace vw;
+
+#include <boost/program_options.hpp>
+namespace po = boost::program_options;
+
+enum SlaveTask {
+  SolveJacobian,
+  SolveUpdateError,
+  Finish
+};
+
 int main ( int argc, char* argv[] ) {
   mpi::environment env(argc,argv);
   mpi::communicator world;
 
-  std::string value;
-  if (world.rank() == 0 ) {
-    value = "Hello, World!";
+  std::srand(time(0)+world.rank());
+
+  if (world.rank() > 0) {
+    vw_out(DebugMessage, "mpi_slave") << "\tSlave " << world.rank()
+                                      << ": Starting\n";
+    int task;
+    while (true) {
+      broadcast(world, task, 0);
+      if ( task == SolveJacobian ) {
+        vw_out(DebugMessage, "mpi_slave") << "\tSlave " << world.rank()
+                                          << " : Solving Jacobian\n";
+        std::vector<int> mydata;
+        for ( uint i = 0; i < world.rank(); i++ )
+          mydata.push_back( std::rand() );
+        gather(world, mydata, 0);
+      } else if ( task == SolveUpdateError ) {
+        vw_out(DebugMessage, "mpi_slave") << "\tSlave " << world.rank() << " : Update Error\n";
+      } else if ( task == Finish ) {
+        vw_out(DebugMessage, "mpi_slave") << "\tSlave " << world.rank() << " : finished\n";
+        return 0;
+      }
+    }
   }
 
-  broadcast(world, value, 0);
+  std::vector<std::string> input_file_names;
 
-  std::cout << "Process #" << world.rank() << " says " << value
-            << std::endl;
+  // Main starts here
+  po::options_description general_options("Options");
+  general_options.add_options()
+    ("help,h", "Display this help message");
+
+  po::options_description hidden_options("");
+  hidden_options.add_options()
+    ("input-files", po::value<std::vector<std::string> >(&input_file_names));
+
+  po::options_description options("Allowed Options");
+  options.add(general_options).add(hidden_options);
+
+  po::positional_options_description p;
+  p.add("input-files", -1);
+
+  std::ostringstream usage;
+  usage << "Usage: " << argv[0] << " [options] <filenames>..." << std::endl << std::endl;
+  usage << general_options << std::endl;
+
+  po::variables_map vm;
+  try {
+    po::store(po::command_line_parser( argc, argv ).options(options).positional(p).run(), vm);
+    po::notify( vm );
+  } catch (po::error &e) {
+    std::cout << "An error occured while parsing command line arguments.\n";
+    std::cout << "\t" << e.what() << "\n\n";
+    std::cout << usage.str();
+    int task = Finish;
+    broadcast(world, task, 0);
+    return 1;
+  }
+
+  if ( vm.count("help") ) {
+    vw_out() << usage.str();
+    int task = Finish;
+    broadcast(world, task, 0);
+    return 1;
+  } else if ( input_file_names.size() < 1 ) {
+    vw_out() << "Forgot to provide input files!\n";
+    vw_out() << usage.str();
+    int task = Finish;
+    broadcast(world, task, 0);
+    return 1;
+  }
+
+  // Performing task
+  int task;
+  for (uint i = 0; i < 3; i++ ) {
+    task = SolveJacobian;
+    Thread::sleep_ms(500);
+    broadcast(world, task, 0);
+    std::vector<std::vector<int> > all_data;
+    std::vector<int> my_data;
+    my_data.push_back(0);
+    my_data.push_back(12);
+    gather(world, my_data, all_data, 0);
+    for ( uint i = 0; i < all_data.size(); i++ ) {
+      vw_out() << "Job " << i << " : produced " << all_data[i].size() << "\n";
+    }
+    vw_out() << "\n";
+
+    Thread::sleep_ms(500);
+    task = SolveUpdateError;
+    broadcast(world, task, 0);
+  }
+  Thread::sleep_ms(500);
+  task = Finish;
+  broadcast(world, task, 0);
 
   return 0;
 }
