bbdbddaefb774962b34116a80a526b4061bc24d7
Zack Moratto <zachary.m.moratto@nasa.gov>
2010-03-22 05:18:41 +0000

    Modified Photometric function to work now
    It should only be taking the difference were both L and R are valid.

--- src/asp/Sessions/ISIS/PhotometricOutlier.cc
+++ src/asp/Sessions/ISIS/PhotometricOutlier.cc
@@ -28,18 +28,21 @@ void vw::photometric_outlier_rejection( std::string const& prefix,
   DiskCacheImageView<PixelGray<float> > right_proj( transform( right_disk_image, trans, ZeroEdgeExtension() ), "tif", TerminalProgressCallback("asp","Projecting R:") );
 
   // Differencing Left and Projected Right
+  ImageViewRef<PixelMask<PixelGray<float32> > > right_mask = create_mask(right_proj);
   DiskImageView<PixelGray<float32> > left_image(prefix+"-L.tif");
-  DiskCacheImageView<PixelGray<float> > diff( abs(left_image-right_proj), "tif", TerminalProgressCallback("asp","Diff:") );
+  DiskCacheImageView<PixelGray<float> > diff( abs(apply_mask(copy_mask(left_image,right_mask))-right_proj), "tif", TerminalProgressCallback("asp","\tDifference:") );
   ChannelAccumulator<math::CDFAccumulator<float32> > cdf;
   for_each_pixel(diff, cdf);
-  float thresh = cdf.quantile(0.99);
+  float thresh = cdf.quantile(0.995); // Pulling out last bin of CDF
+  vw_out() << "\t  Using threshold: " << thresh << "\n";
 
   // Thresholding image and dilating
-  ImageView<PixelGray<float> > dust = threshold(apply_mask(copy_mask(diff,create_mask(right_proj))),thresh,1.0,0.0);
+  ImageView<PixelGray<float> > dust = threshold(apply_mask(copy_mask(diff,right_mask)),thresh,1.0,0.0);
   ImageView<PixelGray<float> > grass;
   grassfire(dust,grass);
+  dust = gaussian_filter(grass,kernel_size/3);
 
-  ImageViewRef<PixelMask<Vector2f > > cleaned_disparity = intersect_mask(disparity_disk_image,intersect_mask(create_mask(threshold(grass,kernel_size,0.0,1.0)),create_mask(right_proj)));
+  ImageViewRef<PixelMask<Vector2f > > cleaned_disparity = intersect_mask(disparity_disk_image,intersect_mask(create_mask(threshold(dust,kernel_size,0.0,1.0)),right_mask));
 
   output_disparity = prefix+"-FDust.tif";
   DiskImageResourceGDAL disparity_rsrc( output_disparity,
