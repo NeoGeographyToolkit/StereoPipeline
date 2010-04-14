16555a857c01164e2ebd3fe2d2f19fee13a5238a
Zack Moratto <zachary.m.moratto@nasa.gov>
2010-04-14 21:46:39 +0000

    Added safety check section to stereo
    It's there to protect the user from themselves.

--- src/asp/Tools/stereo.cc
+++ src/asp/Tools/stereo.cc
@@ -260,6 +260,23 @@ int main(int argc, char* argv[]) {
 #endif
 
   /*********************************************************************************/
+  /*                            user safety check                                  */
+  /*********************************************************************************/
+  {
+    boost::shared_ptr<camera::CameraModel> camera_model1, camera_model2;
+    session->camera_models(camera_model1,camera_model2);
+
+    // Do the camera's appear to be in the same location?
+    if ( norm_2(camera_model1->camera_center(Vector2()) -
+		camera_model2->camera_center(Vector2())) < 1e-3 )
+      vw_out(WarningMessage,"console")
+             << "Your cameras appear to be in the same location!\n"
+	     << "\tYou should be double check your given camera\n"
+	     << "\tmodels as most likely stereo won't be able\n"
+	     << "\tto triangulate or perform epipolar rectification.\n";
+  }
+
+  /*********************************************************************************/
   /*                            preprocessing step                                 */
   /*********************************************************************************/
   if (entry_point <= PREPROCESSING) {
