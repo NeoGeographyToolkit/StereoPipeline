49fc2e91f1e86534e28ab26a0fb997d2e96294be
Zack Moratto <zachary.m.moratto@nasa.gov>
2010-04-20 09:07:25 +0000

    Added skeleton of executables for Photometry Toolkit
    Compiles .. That's about it.

---
+++ src/asp/PhotometryTK/phoinit.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+
+import os, optparse, multiprocessing;
+
+class Usage(Exception):
+    def __init__(self,msg):
+        self.msg = msg
+
+def main():
+    try:
+        try:
+            usage = "phoinit.py [--help] not-sure-yet "
+            parser = optparse.OptionParser(usage=usage);
+
+            (options, args) = parser.parse_args()
+
+            if not args: parser.error("missing files")
+
+        except optparse.OptionError, msg:
+            raise Usage(msg)
+
+        return 0
+
+    except Usage, err:
+        print >>sys.stderr, err.msg
+        return 2
+
+    except Exception, err:
+        sys.stderr.write( str(err) + '\n' )
+        return 1
+
+if __name__ == "__main__":
+    sys.exit(main())
+
---
+++ src/asp/PhotometryTK/phoinitalbedo.cc
@@ -0,0 +1,65 @@
+// __BEGIN_LICENSE__
+// Copyright (C) 2010 United States Government as represented by
+// the Administrator of the National Aeronautics and Space Administration.
+// All Rights Reserved.
+// __END_LICENSE__
+
+// What's this file supposed to do ?
+//
+// (Pho)tometry (Init)ialize (Albedo)
+//
+// This starts the albedo and creates it's platefile.
+// Eq: A = sum( I^k/(R^k*T^k) )
+
+#include <vw/Image.h>
+using namespace vw;
+
+#include <boost/program_options.hpp>
+namespace po = boost::program_options;
+
+using namespace std;
+
+struct Options {
+
+  // For spawning multiple jobs
+  int job_id, num_jobs;
+};
+
+void handle_arguments( int argc, char *argv[], Options& opt ) {
+  po::options_description general_options("");
+  general_options.add_options()
+    ("job_id,j", po::value<int>(&opt.job_id)->default_value(0), "")
+    ("num_jobs,n", po::value<int>(&opt.num_jobs)->default_value(1), "")
+    ("help,h", "Display this help message");
+
+  po::variables_map vm;
+  try {
+    po::store( po::command_line_parser( argc, argv ).options(general_options).run(), vm );
+    po::notify( vm );
+  } catch (po::error &e) {
+    vw_throw( ArgumentErr() << "Error parsing input:\n\t"
+              << e.what() << general_options );
+  }
+
+  std::ostringstream usage;
+  usage << "Usage: " << argv[0] << " [programmer hasn't filled this out]\n";
+
+  if ( vm.count("help") )
+    vw_throw( ArgumentErr() << usage.str() << general_options );
+}
+
+int main( int argc, char *argv[] ) {
+
+  Options opt;
+  try {
+    handle_arguments( argc, argv, opt );
+  } catch ( const ArgumentErr& e ) {
+    vw_out() << e.what() << std::endl;
+    return 1;
+  } catch ( const Exception& e ) {
+    std::cerr << "Error: " << e.what() << std::endl;
+    return 1;
+  }
+
+  return 0;
+}
