736f0d9181496443d1870db492f8c35da1b392b6
Zack Moratto <zachary.m.moratto@nasa.gov>
2010-05-18 18:50:25 +0000

    Added error accumulators
    Includes tests. These are used to make sure that the photometry process is infact improving.

---
+++ src/asp/PhotometryTK/ErrorAccumulators.h
@@ -0,0 +1,49 @@
+#ifndef __ASP_PHO_ERROR_ACCUMULATORS_H__
+#define __ASP_PHO_ERROR_ACCUMULATORS_H__
+
+namespace asp {
+namespace pho {
+
+  // Calculate error
+  class ErrorAccumulator : public vw::ReturnFixedType<void> {
+    double m_error_sum, m_curr_time;
+  public:
+    typedef double value_type;
+    ErrorAccumulator(double t) : m_error_sum(0), m_curr_time(t) {}
+
+    template <class AValT, class RValT>
+    void operator()( AValT const& drg, AValT const& albedo, RValT const& reflectance ) {
+      double i = drg[0], a=albedo[0], s=drg[1], r=reflectance;
+      i -= a*m_curr_time*r;
+      i *= s; i *= i;
+      m_error_sum += i;
+    }
+
+    value_type value() const {
+      return m_error_sum;
+    }
+  };
+
+  // Calculate error
+  class ErrorNRAccumulator : public vw::ReturnFixedType<void> {
+    double m_error_sum, m_curr_time;
+  public:
+    typedef double value_type;
+    ErrorNRAccumulator(double t) : m_error_sum(0), m_curr_time(t) {}
+
+    template <class AValT>
+    void operator()( AValT const& drg, AValT const& albedo ) {
+      double i = drg[0], a=albedo[0], s=drg[1];
+      i -= a*m_curr_time;
+      i *= s; i *= i;
+      m_error_sum += i;
+    }
+
+    value_type value() const {
+      return m_error_sum;
+    }
+  };
+
+}} // end namespace asp::pho
+
+#endif//__ASP_PHO_ERROR_ACCUMULATORS_H__
---
+++ src/asp/PhotometryTK/tests/TestErrorAccumulators.cxx
@@ -0,0 +1,73 @@
+// __BEGIN_LICENSE__
+// Copyright (C) 2006-2009 United States Government as represented by
+// the Administrator of the National Aeronautics and Space Administration.
+// All Rights Reserved.
+// __END_LICENSE__
+
+#include <gtest/gtest.h>
+#include <vw/Image.h>
+#include <asp/PhotometryTK/ErrorAccumulators.h>
+#include <test/Helpers.h>
+
+using namespace vw;
+using namespace asp;
+using namespace asp::pho;
+
+template <class ChannelT>
+class ErrorAccumulatorTest : public ::testing::Test {
+protected:
+  ErrorAccumulatorTest() {}
+
+  virtual void SetUp() {
+    image.set_size(2,2);
+    reflectance.set_size(2,2);
+    albedo.set_size(2,2);
+    exposure_t = 0.5;
+
+    image(0,0) = PixelGrayA<ChannelT>( 1, 1 );
+    image(0,1) = PixelGrayA<ChannelT>( 0, 1 );
+    image(1,0) = PixelGrayA<ChannelT>( 1, 0 );
+    image(1,1) = PixelGrayA<ChannelT>( 2, 4 );
+
+    reflectance(0,0) = 1;
+    reflectance(0,1) = 3;
+    reflectance(1,0) = 0;
+    reflectance(1,1) = 2;
+
+    albedo(0,0) = PixelGrayA<ChannelT>( 2, 1 );
+    albedo(0,1) = PixelGrayA<ChannelT>( 1, 1 );
+    albedo(1,0) = PixelGrayA<ChannelT>( 2, 0 );
+    albedo(1,1) = PixelGrayA<ChannelT>( 3, 2 );
+  }
+
+  ImageView<PixelGrayA<ChannelT> > image, albedo;
+  ImageView<double> reflectance;
+  double exposure_t;
+};
+
+typedef ErrorAccumulatorTest<uint8> ErrorAccumulatorTestInt;
+typedef ErrorAccumulatorTest<float> ErrorAccumulatorTestFloat;
+
+TEST_F( ErrorAccumulatorTestInt, Error ) {
+  ErrorAccumulator eaccum( exposure_t );
+  for_each_pixel(image,albedo,reflectance,eaccum);
+  EXPECT_EQ( 18.25, eaccum.value() );
+}
+
+TEST_F( ErrorAccumulatorTestFloat, Error ) {
+  ErrorAccumulator eaccum( exposure_t );
+  for_each_pixel(image,albedo,reflectance,eaccum);
+  EXPECT_EQ( 18.25, eaccum.value() );
+}
+
+TEST_F( ErrorAccumulatorTestInt, ErrorNR ) {
+  ErrorNRAccumulator eaccum( exposure_t );
+  for_each_pixel(image,albedo,eaccum);
+  EXPECT_EQ( 4.25, eaccum.value() );
+}
+
+TEST_F( ErrorAccumulatorTestFloat, ErrorNR ) {
+  ErrorNRAccumulator eaccum( exposure_t );
+  for_each_pixel(image,albedo,eaccum);
+  EXPECT_EQ( 4.25, eaccum.value() );
+}
