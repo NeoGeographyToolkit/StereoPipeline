fe001d30a6493ccb5b192c84e5f522bd01e50ae8
Zack Moratto <zachary.m.moratto@nasa.gov>
2010-05-22 03:01:53 +0000

    Added 2 Band blending
    This can be used to initialize or for the final last round. It preserves high frequency from the input with the highest alpha.

--- src/asp/PhotometryTK/AlbedoAccumulators.h
+++ src/asp/PhotometryTK/AlbedoAccumulators.h
@@ -179,6 +179,137 @@ namespace pho {
     }
   };
 
+  template <class PixelT>
+  class Albedo2BandAccumulator : public ImageAccumulatorBase<Albedo2BandAccumulator<PixelT> > {
+    typedef typename vw::CompoundChannelCast<PixelT, double>::type accum_pixel_type;
+    typedef typename vw::PixelChannelType<PixelT>::type channel_type;
+    vw::ImageView<double> m_low_f_sum, m_sum_weight;
+    vw::ImageView<accum_pixel_type> m_high_f_sum;
+
+    // Intermediates
+    vw::ImageView<double> m_curr_sum, m_curr_low, m_curr_high;
+
+    void reset( void ) {
+      vw::fill( m_low_f_sum, 0 );
+      vw::fill( m_sum_weight, 0 );
+      vw::fill( m_high_f_sum, accum_pixel_type() );
+    }
+
+    struct UpdateHighFFunctor {
+      template <class Arg1T, class Arg2T, class Arg3T>
+      struct result {
+        typedef Arg1T type;
+      };
+
+      template <class Arg1T, class Arg2T, class Arg3T>
+      inline Arg1T& operator()( Arg1T& high_f_result, Arg2T const& input, Arg3T const& input_alpha  ) const {
+        if ( input_alpha > high_f_result[1] )
+          high_f_result[0] = input;
+        return high_f_result;
+      }
+    };
+
+  public:
+    Albedo2BandAccumulator( vw::int32 const& cols, vw::int32 const& rows ) :
+    m_low_f_sum(cols,rows), m_sum_weight(cols,rows), m_high_f_sum(cols,rows),
+    m_curr_sum(cols,rows), m_curr_low(cols,rows), m_curr_high(cols,rows) {
+      this->reset();
+    }
+
+    // Accumulate
+    template <class IViewT, class RViewT>
+    void operator()( vw::ImageViewBase<IViewT> const& image,
+                     vw::ImageViewBase<RViewT> const& reflectance,
+                     double const& t ) {
+      m_sum_weight += select_channel(image.impl(),1);
+
+      m_curr_sum = vw::channel_cast<double>(select_channel(image.impl(),0))*
+        vw::channel_cast<double>(select_channel(image.impl(),1)) / ( t*reflectance.impl() );
+      m_curr_low = gaussian_filter( m_curr_sum, 2.0 );
+      m_curr_high = m_curr_sum - m_curr_low;
+      m_low_f_sum += m_curr_low;
+
+      // High only makes it in if the input alpha is higher than the currently loaded
+      for_each_pixel( m_high_f_sum, m_curr_high, select_chanel(image.impl(),1) );
+    }
+
+    // Result
+    typedef vw::ImageView<PixelT> result_type;
+    result_type result(void) {
+      // Normalize
+      result_type r = m_low_f_sum/m_sum_weight + m_high_f_sum;
+      // Apply alpha
+      select_channel(r,1) = vw::channel_cast_rescale<channel_type>(threshold(m_sum_weight,0));
+      this->reset();
+      return r;
+    }
+  };
+
+  template <class PixelT>
+  class Albedo2BandNRAccumulator : public ImageAccumulatorBase<Albedo2BandNRAccumulator<PixelT> > {
+    typedef typename vw::CompoundChannelCast<PixelT, double>::type accum_pixel_type;
+    typedef typename vw::PixelChannelType<PixelT>::type channel_type;
+    vw::ImageView<double> m_low_f_sum, m_sum_weight;
+    vw::ImageView<accum_pixel_type> m_high_f_sum;
+
+    // Intermediates
+    vw::ImageView<double> m_curr_sum, m_curr_low, m_curr_high;
+
+    void reset( void ) {
+      vw::fill( m_low_f_sum, 0 );
+      vw::fill( m_sum_weight, 0 );
+      vw::fill( m_high_f_sum, accum_pixel_type() );
+    }
+
+    struct UpdateHighFFunctor {
+      template <class Arg1T, class Arg2T, class Arg3T>
+      struct result {
+        typedef Arg1T type;
+      };
+
+      template <class Arg1T, class Arg2T, class Arg3T>
+      inline Arg1T& operator()( Arg1T& high_f_result, Arg2T const& input, Arg3T const& input_alpha  ) const {
+        if ( input_alpha > high_f_result[1] )
+          high_f_result[0] = input;
+        return high_f_result;
+      }
+    };
+
+  public:
+    Albedo2BandNRAccumulator( vw::int32 const& cols, vw::int32 const& rows ) :
+    m_low_f_sum(cols,rows), m_sum_weight(cols,rows), m_high_f_sum(cols,rows),
+    m_curr_sum(cols,rows), m_curr_low(cols,rows), m_curr_high(cols,rows) {
+      this->reset();
+    }
+
+    // Accumulate
+    template <class IViewT>
+    void operator()( vw::ImageViewBase<IViewT> const& image,
+                     double const& t ) {
+      m_sum_weight += select_channel(image.impl(),1);
+
+      m_curr_sum = vw::channel_cast<double>(select_channel(image.impl(),0))*
+        vw::channel_cast<double>(select_channel(image.impl(),1)) / t;
+      m_curr_low = gaussian_filter( m_curr_sum, 2.0 );
+      m_curr_high = m_curr_sum - m_curr_low;
+      m_low_f_sum += m_curr_low;
+
+      // High only makes it in if the input alpha is higher than the currently loaded
+      for_each_pixel( m_high_f_sum, m_curr_high, select_channel(image.impl(),1), UpdateHighFFunctor() );
+    }
+
+    // Result
+    typedef vw::ImageView<PixelT> result_type;
+    result_type result(void) {
+      // Normalize
+      result_type r = m_low_f_sum/m_sum_weight + m_high_f_sum;
+      // Apply alpha
+      select_channel(r,1) = vw::channel_cast_rescale<channel_type>(threshold(m_sum_weight,0));
+      this->reset();
+      return r;
+    }
+  };
+
 }} // end namespace asp::pho
 
 #endif//__ASP_PHO_ALBEDO_ACCUMULATORS_H__
