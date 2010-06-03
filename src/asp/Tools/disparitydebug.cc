1dbd8ef1d8f3739cb4ac7d3736acf4de941de890
Zack Moratto <zachary.m.moratto@nasa.gov>
2010-06-03 17:44:14 +0000

    Disparity switched to new excutable format
    New format has handling_arguments() and also has all important code wrapped in a try catch.

--- src/asp/Tools/disparitydebug.cc
+++ src/asp/Tools/disparitydebug.cc
@@ -28,6 +28,7 @@ namespace fs = boost::filesystem;
 #include <vw/FileIO.h>
 #include <vw/Image.h>
 #include <vw/Stereo/DisparityMap.h>
+#include <asp/Core/Macros.h>
 using namespace vw;
 using namespace vw::stereo;
 
@@ -36,68 +37,89 @@ namespace vw {
   template<> struct PixelFormatID<Vector3>   { static const PixelFormatEnum value = VW_PIXEL_GENERIC_3_CHANNEL; };
 }
 
-int main( int argc, char *argv[] ) {
-  std::string input_file_name, output_prefix = "", output_file_type;
+struct Options {
+  // Input
+  std::string input_file_name;
 
-  po::options_description desc("Options");
-  desc.add_options()
-    ("help,h", "Display this help message")
-    ("input-file", po::value<std::string>(&input_file_name), "Explicitly specify the input file")
-    ("output-prefix,o", po::value<std::string>(&output_prefix), "Specify the output prefix")
-    ("output-filetype,t", po::value<std::string>(&output_file_type)->default_value("tif"), "Specify the output file")
-    ("float-pixels", "Save the resulting debug images as 32 bit floating point files (if supported by the selected files type.");
+  // Output
+  bool float_output;
+  std::string output_prefix, output_file_type;
+};
 
-  po::positional_options_description p;
-  p.add("input-file", 1);
+void handle_arguments( int argc, char *argv[], Options& opt ) {
+  po::options_description general_options("");
+  general_options.add_options()
+    ("output-prefix,o", po::value(&opt.output_prefix), "Specify the output prefix")
+    ("output-filetype,t", po::value(&opt.output_file_type)->default_value("tif"), "Specify the output file")
+    ("float-pixels", "Save the resulting debug images as 32 bit floating point files (if supported by the selected files type.")
+    ("help,h", "Display this help message");
+
+  po::options_description positional("");
+  positional.add_options()
+    ("input-file", po::value(&opt.input_file_name), "Input disparity map");
+
+  po::positional_options_description positional_desc;
+  positional_desc.add("input-file", 1);
+
+  po::options_description all_options;
+  all_options.add(general_options).add(positional);
 
   po::variables_map vm;
   try {
-    po::store( po::command_line_parser( argc, argv ).options(desc).positional(p).run(), vm );
+    po::store( po::command_line_parser( argc, argv ).options(all_options).positional(positional_desc).run(), vm );
     po::notify( vm );
   } catch (po::error &e) {
-    std::cout << "An error occured while parsing command line arguments.\n";
-    std::cout << "\t" << e.what() << "\n\n";
-    std::cout << desc << std::endl;
-    return 1;
+    vw_throw( ArgumentErr() << "Error parsing input:\n\t"
+              << e.what() << general_options );
   }
 
-  if( vm.count("help") ) {
-    std::cout << desc << std::endl;
-    return 1;
-  }
+  std::ostringstream usage;
+  usage << "Usage: " << argv[0] << " [options] <input disparity map> \n";
 
-  if( vm.count("input-file") != 1 ) {
-    std::cout << "Error: Must specify exactly one input file!" << std::endl;
-    std::cout << desc << std::endl;
-    return 1;
-  }
+  if ( vm.count("help") )
+    vw_throw( ArgumentErr() << usage.str() << general_options );
+  if ( opt.input_file_name.empty() )
+    vw_throw( ArgumentErr() << "Missing input file!\n"
+              << usage.str() << general_options );
+  if ( opt.output_prefix == "" )
+    opt.output_prefix = fs::path(opt.input_file_name).stem();
+  opt.float_output = vm.count("float-pixels");
+}
 
-  if( output_prefix == "" ) {
-    output_prefix = fs::path(input_file_name).stem();
-  }
+int main( int argc, char *argv[] ) {
 
-  vw_out() << "Opening " << input_file_name << "\n";
-  DiskImageView<PixelMask<Vector2f> > disk_disparity_map(input_file_name);
+  Options opt;
+  try {
+    handle_arguments( argc, argv, opt );
 
-  vw_out() << "\t--> Computing disparity range \n";
-  BBox2 disp_range = get_disparity_range(disk_disparity_map);
-  vw_out() << "\t    Horizontal - [" << disp_range.min().x() << " " << disp_range.max().x() << "]    Vertical: [" << disp_range.min().y() << " " << disp_range.max().y() << "]\n";
+    vw_out() << "Opening " << opt.input_file_name << "\n";
+    DiskImageView<PixelMask<Vector2f> > disk_disparity_map(opt.input_file_name);
 
-  ImageViewRef<float32> horizontal = apply_mask(copy_mask(clamp(normalize(select_channel(disk_disparity_map,0), disp_range.min().x(), disp_range.max().x(),0,1)),disk_disparity_map));
-  ImageViewRef<float32> vertical = apply_mask(copy_mask(clamp(normalize(select_channel(disk_disparity_map,1), disp_range.min().y(), disp_range.max().y(),0,1)),disk_disparity_map));
+    vw_out() << "\t--> Computing disparity range \n";
+    BBox2 disp_range = get_disparity_range(disk_disparity_map);
+    vw_out() << "\t    Horizontal - [" << disp_range.min().x()
+             << " " << disp_range.max().x() << "]    Vertical: ["
+             << disp_range.min().y() << " " << disp_range.max().y() << "]\n";
 
-  vw_out() << "\t--> Saving disparity debug images\n";
-  if (vm.count("float-pixels")) {
-    write_image( output_prefix + "-H." + output_file_type, horizontal,
-                 TerminalProgressCallback("asp","\t    Left  : "));
-    write_image( output_prefix + "-V." + output_file_type, vertical,
-                 TerminalProgressCallback("asp","\t    Right : "));
-  } else {
-    write_image( output_prefix + "-H." + output_file_type, channel_cast_rescale<uint8>(horizontal),
-                 TerminalProgressCallback("asp","\t    Left  : "));
-    write_image( output_prefix + "-V." + output_file_type, channel_cast_rescale<uint8>(vertical),
-                 TerminalProgressCallback("asp","\t    Right : "));
-  }
+    ImageViewRef<float32> horizontal = apply_mask(copy_mask(clamp(normalize(select_channel(disk_disparity_map,0), disp_range.min().x(), disp_range.max().x(),0,1)),disk_disparity_map));
+    ImageViewRef<float32> vertical = apply_mask(copy_mask(clamp(normalize(select_channel(disk_disparity_map,1), disp_range.min().y(), disp_range.max().y(),0,1)),disk_disparity_map));
+
+    vw_out() << "\t--> Saving disparity debug images\n";
+    if (opt.float_output) {
+      write_image( opt.output_prefix + "-H." + opt.output_file_type, horizontal,
+                   TerminalProgressCallback("asp","\t    Left  : "));
+      write_image( opt.output_prefix + "-V." + opt.output_file_type, vertical,
+                   TerminalProgressCallback("asp","\t    Right : "));
+    } else {
+      write_image( opt.output_prefix + "-H." + opt.output_file_type,
+                   channel_cast_rescale<uint8>(horizontal),
+                   TerminalProgressCallback("asp","\t    Left  : "));
+      write_image( opt.output_prefix + "-V." + opt.output_file_type,
+                   channel_cast_rescale<uint8>(vertical),
+                   TerminalProgressCallback("asp","\t    Right : "));
+    }
+
+  } ASP_STANDARD_CATCHES;
 
   return 0;
 }
