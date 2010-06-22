2b892e6b39059ed080e39433dea6562d54cc8636
Zack Moratto <zachary.m.moratto@nasa.gov>
2010-06-21 22:21:24 -0700

    Compress IsisAdjustCameraModel to use TEST_F
    It saves some lines. Hooray.

--- src/asp/IsisIO/tests/TestIsisAdjustCameraModel.cxx
+++ src/asp/IsisIO/tests/TestIsisAdjustCameraModel.cxx
@@ -8,65 +8,84 @@
 #include <gtest/gtest.h>
 
 #include <asp/IsisIO/IsisAdjustCameraModel.h>
+#include <asp/IsisIO/IsisCameraModel.h>
+#include <test/Helpers.h>
+#include <boost/foreach.hpp>
 
 using namespace vw;
 using namespace vw::camera;
 using namespace asp;
 
-Vector2 generate_random( int const& xsize,
-                         int const& ysize ) {
-  Vector2 pixel;
-  pixel[0] = rand() % ( 10 * xsize - 10 ) + 10;
-  pixel[0] /= 10;
-  pixel[1] = rand() % ( 10 * ysize - 10 ) + 10;
-  pixel[1] /= 10;
-  return pixel;
-}
+class IsisAdjustCameraTest : public ::testing::Test {
+protected:
+  IsisAdjustCameraTest() {}
 
-TEST(IsisAdjustCameraModel, NoFunctions) {
+  virtual void SetUp() {
+    files.push_back("E1701676.reduce.cub");
+    files.push_back("5165r.cub");
+  }
+
+  Vector2 generate_random( int const& xsize,
+                           int const& ysize ) {
+    Vector2 pixel;
+    pixel[0] = rand() % ( 10 * xsize - 10 ) + 10;
+    pixel[0] /= 10;
+    pixel[1] = rand() % ( 10 * ysize - 10 ) + 10;
+    pixel[1] /= 10;
+    return pixel;
+  }
+
+  void create_pixels( IsisAdjustCameraModel const& cam ) {
+    pixels.clear();
+    points.clear();
+    for ( unsigned i = 0; i < 100; i++ ) {
+      pixels.push_back( generate_random(cam.samples(),cam.lines()) );
+      points.push_back( cam.pixel_to_vector(pixels.back()) );
+      points.back() *= 100000; // 100 km
+      points.back() += cam.camera_center(pixels.back());
+    }
+  }
+
+  // Feed the camera noise to make sure we are not using stored
+  // values.
+  void fuzz_camera( IsisAdjustCameraModel const& cam ) {
+    Vector2 noise = generate_random( cam.samples(),
+                                     cam.lines() );
+    Vector3 temp = cam.pixel_to_vector( noise );
+  }
+
+  std::vector<Vector2> pixels;
+  std::vector<Vector3> points;
   std::vector<std::string> files;
-  files.push_back("E1701676.reduce.cub");
-  files.push_back("5165r.cub");
-  srand( time(NULL) );
-  for ( uint j = 0; j < files.size(); j++ ) {
+};
+
+TEST_F(IsisAdjustCameraTest, NoFunctions) {
+  BOOST_FOREACH( std::string const& cube, files ) {
     boost::shared_ptr<BaseEquation> blank( new PolyEquation(0) );
-    IsisAdjustCameraModel cam( files[j], blank, blank );
+    IsisAdjustCameraModel cam( cube, blank, blank );
+    IsisCameraModel noa_cam( cube );
+    create_pixels( cam );
+    fuzz_camera( cam );
 
-    std::cout << "File: " << files[j] << "\n";
-    std::cout << "-------------------------------------\n";
+    for ( unsigned i = 0; i < pixels.size(); i++ ) {
+      // Test Position hasn't changed
+      fuzz_camera( cam );
+      EXPECT_VECTOR_NEAR( cam.camera_center( pixels[i] ),
+                          noa_cam.camera_center( pixels[i] ),
+                          0.001 );
+      EXPECT_MATRIX_NEAR( cam.camera_pose( pixels[i] ).rotation_matrix(),
+                          noa_cam.camera_pose( pixels[i] ).rotation_matrix(),
+                          0.001 );
 
-    for ( uint i = 0; i < 100; i++ ) {
-      Vector2 pixel = generate_random( cam.samples(),
-                                       cam.lines() );
-      Vector3 point = cam.pixel_to_vector( pixel );
-      for ( uint k = 0; k < 5; k++ ) {
-        // Apply noise to make sure we are not using stored values
-        Vector2 noise = generate_random( cam.samples(),
-                                         cam.lines() );
-        Vector3 temp = cam.pixel_to_vector( noise );
-      }
-      point *= 100000; // 100 km below
-      point += cam.camera_center( pixel );
-      for ( uint k = 0; k < 5; k++ ) {
-        // Apply noise to make sure we are not using stored values
-        Vector2 noise = generate_random( cam.samples(),
-                                         cam.lines() );
-        Vector3 temp = cam.camera_center( noise );
-      }
-
-      Vector2 rpixel = cam.point_to_pixel( point );
-      EXPECT_NEAR( pixel[0], rpixel[0], 0.001 );
-      EXPECT_NEAR( pixel[1], rpixel[1], 0.001 );
+      // Test Circle Projection
+      Vector2 rpixel = cam.point_to_pixel( points[i] );
+      EXPECT_VECTOR_NEAR( pixels[i], rpixel, 0.001 );
     }
   }
 }
 
-TEST(IsisAdjustCameraModel, PolyFunctions) {
-  std::vector<std::string> files;
-  files.push_back("E1701676.reduce.cub");
-  files.push_back("5165r.cub");
-  srand( time(NULL) );
-  for ( uint j = 0; j < files.size(); j++ ) {
+TEST_F(IsisAdjustCameraTest, PolyFunctions) {
+  BOOST_FOREACH( std::string const& cube, files ) {
     boost::shared_ptr<BaseEquation> position( new PolyEquation(1) );
     (*position)[0] = 1000;
     (*position)[1] = 10;
@@ -78,54 +97,28 @@ TEST(IsisAdjustCameraModel, PolyFunctions) {
     (*position)[0] = .07;
     (*position)[1] = -.1;
     (*position)[2] = .02;
-    IsisAdjustCameraModel cam( files[j], position, pose );
-    std::cout << "File: " << files[j] << "\n";
-    std::cout << "---------------------------------------\n";
+    IsisAdjustCameraModel cam( cube, position, pose );
+    create_pixels( cam );
+    fuzz_camera( cam );
 
-    for ( uint i = 0; i < 100; i++ ) {
-      Vector2 pixel = generate_random( cam.samples(),
-                                       cam.lines() );
-      Vector3 point = cam.pixel_to_vector( pixel );
-      for ( uint k = 0; k < 5; k++ ) {
-        // Apply noise
-        Vector2 noise = generate_random( cam.samples(),
-                                         cam.lines() );
-        Vector3 temp = cam.pixel_to_vector( noise );
-      }
-      point *= 100000; // 100 km
-      point += cam.camera_center( pixel );
-      for ( uint k = 0; k < 5; k++ ) {
-        // Apply noise
-        Vector2 noise = generate_random( cam.samples(),
-                                         cam.lines() );
-        Vector3 temp = cam.camera_center( noise );
-      }
-      Vector2 rpixel = cam.point_to_pixel( point );
-      EXPECT_NEAR( pixel[0], rpixel[0], 0.001 );
-      EXPECT_NEAR( pixel[1], rpixel[1], 0.001 );
+    for ( unsigned i = 0; i < pixels.size(); i++ ) {
+      Vector2 rpixel = cam.point_to_pixel( points[i] );
+      EXPECT_VECTOR_NEAR( pixels[i], rpixel, 0.001 );
       (*position)[2] += 1000;
       Vector3 rpoint = cam.pixel_to_vector( rpixel );
-      for ( uint k = 0; k < 5; k++ ) {
-        // Apply noise
-        Vector2 noise = generate_random( cam.samples(),
-                                         cam.lines() );
-        Vector3 temp = cam.pixel_to_vector( noise );
-      }
-      rpoint *= 100000; // 100 km
+      fuzz_camera(cam);
+      rpoint *= 100000;
       rpoint += cam.camera_center( rpixel );
-      EXPECT_NEAR( rpoint[0] - point[0], 0, .001 );
-      EXPECT_NEAR( rpoint[1] - point[1], 1000, .001 );
-      EXPECT_NEAR( rpoint[2] - point[2], 0, .001 );
+      EXPECT_NEAR( rpoint[0] - points[i][0], 0, .001 );
+      EXPECT_NEAR( rpoint[1] - points[i][1], 1000, .001 );
+      EXPECT_NEAR( rpoint[2] - points[i][2], 0, .001 );
+      (*position)[2] -= 1000;
     }
   }
 }
 
-TEST(IsisAdjustCameraModel, RPNFunctions) {
-  std::vector<std::string> files;
-  files.push_back("E1701676.reduce.cub");
-  files.push_back("5165r.cub");
-  srand( time(NULL) );
-  for ( uint j = 0; j < files.size(); j++ ) {
+TEST_F(IsisAdjustCameraTest, RPNFunctions) {
+  BOOST_FOREACH( std::string const& cube, files ) {
     std::string xpos_eq = "t 2 * 100 / 99 +";
     std::string ypos_eq = "t .8 * 1000 -";
     std::string zpos_eq = "t .5 * 2000 +";
@@ -138,46 +131,22 @@ TEST(IsisAdjustCameraModel, RPNFunctions) {
     boost::shared_ptr<BaseEquation> pose( new RPNEquation( xang_eq,
                                                            yang_eq,
                                                            zang_eq ) );
-    IsisAdjustCameraModel cam( files[j], position, pose );
-    std::cout << "File: " << files[j] << "\n";
-    std::cout << "---------------------------------------\n";
-    std::cout << "Position ET offset: " << position->get_time_offset() << "\n";
-    std::cout << "Pose ET offset: " << pose->get_time_offset() << "\n";
+    IsisAdjustCameraModel cam( cube, position, pose );
+    create_pixels( cam );
+    fuzz_camera( cam );
 
-    for ( uint i = 0; i < 2; i++ ) {
-      Vector2 pixel = generate_random( cam.samples(),
-                                       cam.lines() );
-      Vector3 point = cam.pixel_to_vector( pixel );
-      for ( uint k = 0; k < 5; k++ ) {
-        // Apply noise
-        Vector2 noise = generate_random( cam.samples(),
-                                         cam.lines() );
-        Vector3 temp = cam.pixel_to_vector( noise );
-      }
-      point *= 80000; // 80 km
-      point += cam.camera_center( pixel );
-      for ( uint k = 0; k < 5; k++ ) {
-        // Apply noise
-        Vector2 noise = generate_random( cam.samples(),
-                                         cam.lines() );
-        Vector3 temp = cam.camera_center( noise );
-      }
-      Vector2 rpixel = cam.point_to_pixel( point );
-      EXPECT_NEAR( pixel[0], rpixel[0], 0.001 );
-      EXPECT_NEAR( pixel[1], rpixel[1], 0.001 );
+    for ( unsigned i = 0; i < pixels.size(); i++ ) {
+      Vector2 rpixel = cam.point_to_pixel( points[i] );
+      EXPECT_VECTOR_NEAR( pixels[i], rpixel, 0.001 );
       (*position)[2] -= 500;
       Vector3 rpoint = cam.pixel_to_vector( rpixel );
-      for ( uint k = 0; k < 5; k++ ) {
-        // Apply noise
-        Vector2 noise = generate_random( cam.samples(),
-                                         cam.lines() );
-        Vector3 temp = cam.pixel_to_vector( noise );
-      }
-      rpoint *= 80000; // 80 km
+      fuzz_camera( cam );
+      rpoint *= 100000;
       rpoint += cam.camera_center( rpixel );
-      EXPECT_NEAR( rpoint[0] - point[0], -500, .001 );
-      EXPECT_NEAR( rpoint[1] - point[1], 0, .001 );
-      EXPECT_NEAR( rpoint[2] - point[2], 0, .001 );
+      EXPECT_NEAR( rpoint[0] - points[i][0], -500, .001 );
+      EXPECT_NEAR( rpoint[1] - points[i][1], 0, .001 );
+      EXPECT_NEAR( rpoint[2] - points[i][2], 0, .001 );
+      (*position)[2] += 500;
     }
   }
 }
