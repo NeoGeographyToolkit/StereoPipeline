a9b133867a1595abb023137d57753c0988c9885c
Zack Moratto <zachary.m.moratto@nasa.gov>
2011-03-25 11:29:04 -0700

    Make orthoproject return same type as input
    Except I always append alpha. I'm not sure if this is a good idea.
    
    Also, not being able to block write with geospatial information
    totally sucks. I should look into this.

--- src/asp/Tools/orthoproject.cc
+++ src/asp/Tools/orthoproject.cc
@@ -7,16 +7,6 @@
 
 /// \file othoproject.cc
 ///
-#include <boost/algorithm/string.hpp>
-#include <boost/program_options.hpp>
-#include <boost/filesystem/path.hpp>
-#include <boost/filesystem/operations.hpp>
-#include <boost/filesystem/convenience.hpp>
-#include <boost/filesystem/fstream.hpp>
-using namespace boost;
-namespace po = boost::program_options;
-namespace fs = boost::filesystem;
-
 #include <vw/Core.h>
 #include <vw/Image.h>
 #include <vw/FileIO.h>
@@ -28,73 +18,14 @@ using namespace vw::camera;
 using namespace vw::cartography;
 
 #include <asp/Core/Macros.h>
+#include <asp/Core/Common.h>
 #include <asp/Sessions.h>
 #include <asp/IsisIO/DiskImageResourceIsis.h>
 #include <boost/tokenizer.hpp>
+namespace po = boost::program_options;
+namespace fs = boost::filesystem;
 
-template <class ViewT, class DemViewT>
-GeoReference compute_geotransform_from_camera(ImageViewBase<ViewT> const& view,
-                                              boost::shared_ptr<CameraModel> camera,
-                                              ImageViewBase<DemViewT> const& dem,
-                                              GeoReference const& dem_georef,
-                                              double scale,
-                                              double &output_width, double& output_height) {
-
-  // Adopt the datum & projection of the parent DEM.
-  GeoReference geo = dem_georef;
-
-  // Compute the longitude/latitude bounding box and the lonlat pixel scale
-  float lonlat_scale;
-  BBox2 image_bbox = camera_bbox(dem_georef, camera, view.impl().cols(), view.impl().rows(), lonlat_scale);
-  BBox2 dem_bbox = dem_georef.lonlat_bounding_box(dem);
-
-  // Use a bbox where both the image and the DEM have valid data.
-  // Throw an error if there turns out to be no overlap.
-  BBox2 bbox = image_bbox;
-  bbox.crop(dem_bbox);
-
-  if (bbox.width() == 0 || bbox.height() == 0) {
-    vw_out() << "Image bounding box (" << image_bbox << ") and DEM bounding box (" << dem_bbox << ") have no overlap.  Are you sure that your input files overlap?\n";
-    return 0;
-  }
-
-  double diff1 = bbox.max().x()-bbox.min().x();
-
-  // Convert the lon/lat bounding box into the map projection.
-  bbox.min() = geo.lonlat_to_point(bbox.min());
-  bbox.max() = geo.lonlat_to_point(bbox.max());
-  double diff2 = bbox.max().x()-bbox.min().x();
-
-  // Convert the scale into the projected space
-  double projected_space_scale = lonlat_scale * diff2 / diff1;
-
-  // If the user has supplied a scale, we override our computed value
-  // here.
-  if (scale != 0)
-    projected_space_scale = scale;
-
-  // Compute the affine transform for this image.
-  Matrix3x3 trans = geo.transform();
-
-  trans(0,0) = projected_space_scale;
-  trans(1,1) = -projected_space_scale;
-  trans(0,2) = bbox.min().x();
-  trans(1,2) = bbox.max().y();
-  geo.set_transform(trans);
-
-  // Compute the width and height of the output image so that it
-  // contains the entirety of the computed bounding box.
-
-  //***** FIXME: This seems backwards to me, which probably means that
-  //***** lonlat_bounding_box() is backwards, but I need to get this
-  //***** to work right now so I'll investigate later.
-  output_height = (bbox.max().x() - bbox.min().x()) / projected_space_scale;
-  output_width = (bbox.max().y() - bbox.min().y()) / projected_space_scale;
-
-  return geo;
-}
-
-struct Options {
+struct Options : asp::BaseOptions {
   Options() : lo(0), hi(0), do_color(false) {
     nodata_value = mpp = ppd = std::numeric_limits<double>::quiet_NaN();
   }
@@ -121,8 +52,8 @@ void handle_arguments( int argc, char *argv[], Options& opt ) {
     ("min", po::value(&opt.lo), "Explicitly specify the range of the normalization (for ISIS images only)")
     ("max", po::value(&opt.hi), "Explicitly specify the range of the normalization (for ISIS images only)")
     ("session-type,t", po::value(&opt.stereo_session), "Select the stereo session type to use for processing. [default: pinhole]")
-    ("use-solid-color", po::value(&color_text), "Use a solid color instead of camera image. Example: 255,0,128")
-    ("help,h", "Display this help message");
+    ("use-solid-color", po::value(&color_text), "Use a solid color instead of camera image. Example: 255,0,128");
+  general_options.add( asp::BaseOptionsDescription(opt) );
 
   po::options_description positional("");
   positional.add_options()
@@ -137,23 +68,13 @@ void handle_arguments( int argc, char *argv[], Options& opt ) {
   positional_desc.add("camera-model",1);
   positional_desc.add("output-file",1);
 
-  po::options_description all_options;
-  all_options.add(general_options).add(positional);
-
-  po::variables_map vm;
-  try {
-    po::store( po::command_line_parser( argc, argv ).options(all_options).positional(positional_desc).run(), vm );
-    po::notify( vm );
-  } catch (po::error &e) {
-    vw_throw( ArgumentErr() << "Error parsing input:\n"
-              << e.what() << "\n" << general_options );
-  }
-
   std::ostringstream usage;
   usage << "Usage: " << argv[0] << " [options] <dem> <camera-image> <camera-model> <output>\n";
 
-  if ( vm.count("help") )
-    vw_throw( ArgumentErr() << usage.str() << general_options );
+  po::variables_map vm =
+    asp::check_command_line( argc, argv, opt, general_options,
+                             positional, positional_desc, usage.str() );
+
   if ( opt.dem_file.empty() || opt.image_file.empty() ||
        opt.camera_model_file.empty() )
     vw_throw( ArgumentErr() << "Missing input files!\n"
@@ -176,6 +97,60 @@ void handle_arguments( int argc, char *argv[], Options& opt ) {
   } else {
     opt.do_color=false;
   }
+}
+
+template <class PixelT>
+void do_projection( Options& opt,
+                    ImageViewRef<PixelMask<float> > const& dem,
+                    GeoReference const& drg_georef,
+                    boost::shared_ptr<camera::CameraModel> camera_model ) {
+  DiskImageView<PixelT > texture_disk_image(opt.image_file);
+  ImageViewRef<PixelT > texture_image = texture_disk_image;
+  DiskImageView<float > float_texture_disk_image(opt.image_file);
+
+#if defined(ASP_HAVE_PKG_ISISIO) && ASP_HAVE_PKG_ISISIO == 1
+  // ISIS cubes need to be normalized because their pixels are often
+  // photometrically calibrated.
+  if ( opt.stereo_session == "isis" && !opt.do_color ) {
+    DiskImageResourceIsis isis_rsrc( opt.image_file);
+    if (opt.lo == 0 && opt.hi == 0) {
+      vw_out(InfoMessage) << "\t--> Computing statistics for ISIS image\n";
+      int32 left_stat_scale =
+        int32(ceil(sqrt(float(texture_image.cols())*
+                        float(texture_image.rows()) / 1000000)));
+      ChannelAccumulator<vw::math::CDFAccumulator<float> > accumulator;
+      for_each_pixel(
+                     subsample(create_mask( float_texture_disk_image,
+                                            float(isis_rsrc.valid_minimum()),
+                                            float(isis_rsrc.valid_maximum())),
+                               left_stat_scale ),
+                     accumulator );
+      opt.lo = accumulator.approximate_mean() -
+        2*accumulator.approximate_stddev();
+      opt.hi = accumulator.approximate_mean() +
+        2*accumulator.approximate_stddev();
+      vw_out() << "\t--> Normalizing ISIS pixel range range: ["
+               << opt.lo << "  " << opt.hi << "]\n";
+    } else {
+      vw_out() << "\t--> Using user-specified normalization range: ["
+               << opt.lo << "  " << opt.hi << "]\n";
+    }
+    texture_image =
+      pixel_cast_rescale<PixelT >(normalize(remove_isis_special_pixels(float_texture_disk_image, opt.lo), opt.lo, opt.hi, 0, 1));
+  }
+#endif
+
+
+  vw_out() << "\t--> Orthoprojecting texture.\n";
+  ImageViewRef<PixelT > final_result =
+    orthoproject(dem, drg_georef,
+                 texture_image, camera_model,
+                 BicubicInterpolation(), ZeroEdgeExtension());
+
+  DiskImageResourceGDAL* rsrc = asp::build_gdal_rsrc(opt.output_file, final_result, opt );
+  write_georeference(*rsrc, drg_georef);
+  write_image(*rsrc, final_result, TerminalProgressCallback("asp",""));
+  delete rsrc;
 }
 
 int main(int argc, char* argv[]) {
@@ -234,7 +209,7 @@ int main(int argc, char* argv[]) {
     camera_model = session->camera_model(opt.image_file, opt.camera_model_file);
 
     GeoReference dem_georef;
-    ImageViewRef<PixelMask<PixelGray<float> > > dem;
+    ImageViewRef<PixelMask<float > > dem;
 
     if ( fs::path(opt.dem_file).extension() == "" ) {
       // This option allows the user to just project directly unto to
@@ -251,14 +226,14 @@ int main(int argc, char* argv[]) {
                      Matrix3x3(1, 0, (llr_camera_loc[0] < 90 ||
                                       llr_camera_loc[0] > 270) ? -180 : 0,
                                0, -1, 90, 0, 0, 1) );
-      dem = constant_view(PixelMask<PixelGray<float> >(0), 360, 180 );
+      dem = constant_view(PixelMask<float>(0), 360, 180 );
       vw_out() << "\t--> Using flat datum \"" << opt.dem_file << "\" as elevation model.\n";
     } else {
       // Open the DEM
       cartography::read_georeference(dem_georef, opt.dem_file);
 
-      DiskImageView<PixelGray<float> > dem_disk_image(opt.dem_file);
-      dem = pixel_cast<PixelMask<PixelGray<float> > >(dem_disk_image);
+      DiskImageView<float> dem_disk_image(opt.dem_file);
+      dem = pixel_cast<PixelMask<float> >(dem_disk_image);
 
       // Attempting to extract automatic DEM nodata value.
       if ( std::isnan(opt.nodata_value) ) {
@@ -273,43 +248,6 @@ int main(int argc, char* argv[]) {
         dem = create_mask(dem_disk_image, opt.nodata_value);
       }
     }
-
-    // This is a bad idea. We should switch like image2qtree.
-    DiskImageView<PixelGrayA<uint8> > texture_disk_image(opt.image_file);
-    ImageViewRef<PixelGrayA<uint8> > texture_image = texture_disk_image;
-    DiskImageView<float > float_texture_disk_image(opt.image_file);
-
-#if defined(ASP_HAVE_PKG_ISISIO) && ASP_HAVE_PKG_ISISIO == 1
-    // ISIS cubes need to be normalized because their pixels are often
-    // photometrically calibrated.
-    if ( opt.stereo_session == "isis" ) {
-      DiskImageResourceIsis isis_rsrc( opt.image_file);
-      if (opt.lo == 0 && opt.hi == 0) {
-        vw_out(InfoMessage) << "\t--> Computing statistics for ISIS image\n";
-        int32 left_stat_scale =
-          int32(ceil(sqrt(float(texture_image.cols())*
-                          float(texture_image.rows()) / 1000000)));
-        ChannelAccumulator<vw::math::CDFAccumulator<float> > accumulator;
-        for_each_pixel(
-                       subsample(create_mask( float_texture_disk_image,
-                                              float(isis_rsrc.valid_minimum()),
-                                              float(isis_rsrc.valid_maximum())),
-                                 left_stat_scale ),
-                       accumulator );
-        opt.lo = accumulator.approximate_mean() -
-          2*accumulator.approximate_stddev();
-        opt.hi = accumulator.approximate_mean() +
-          2*accumulator.approximate_stddev();
-        vw_out() << "\t--> Normalizing ISIS pixel range range: ["
-                 << opt.lo << "  " << opt.hi << "]\n";
-      } else {
-        vw_out() << "\t--> Using user-specified normalization range: ["
-                 << opt.lo << "  " << opt.hi << "]\n";
-      }
-      texture_image =
-        pixel_cast_rescale<PixelGrayA<uint8> >(normalize(remove_isis_special_pixels(float_texture_disk_image, opt.lo), opt.lo, opt.hi, 0, 1));
-    }
-#endif
 
     // Parse the scale of the output image from the command line
     // arguments.  This value can be set either in meters per pixel or
@@ -340,10 +278,13 @@ int main(int argc, char* argv[]) {
     BBox2 projection_bbox, dem_bbox;
     {
       vw_out() << "\t--> Extracting camera bbox ... " << std::flush;
+      SrcImageResource *texture_rsrc =
+        DiskImageResource::open(opt.image_file);
       float mpp_auto_scale;
       projection_bbox =
-        camera_bbox( dem, dem_georef, camera_model, texture_image.cols(),
-                     texture_image.rows(), mpp_auto_scale );
+        camera_bbox( dem, dem_georef, camera_model, texture_rsrc->cols(),
+                     texture_rsrc->rows(), mpp_auto_scale );
+      delete texture_rsrc;
       dem_bbox = dem_georef.bounding_box(dem);
       // if ( projection_bbox.min()[0] < 0 ) {
       //   projection_bbox.min() += Vector2(360,0);
@@ -386,32 +327,65 @@ int main(int argc, char* argv[]) {
     drg_georef.set_transform(drg_trans);
 
     GeoTransform trans(dem_georef, drg_georef);
-    ImageViewRef<PixelMask<PixelGray<float> > > output_dem =
+    ImageViewRef<PixelMask<float> > output_dem =
       crop(transform(dem, trans,
                      ZeroEdgeExtension(),
                      BicubicInterpolation()),
            BBox2i(0,0,int32(output_width),int32(output_height)));
 
+    SrcImageResource *texture_rsrc =
+      DiskImageResource::open(opt.image_file);
+    ImageFormat fmt = texture_rsrc->format();
+    delete texture_rsrc;
     if ( opt.do_color ) {
       vw_out() << "\t--> Orthoprojecting solid color image.\n";
-      ImageViewRef<PixelGrayA<uint8> > final_result =
+      ImageViewRef<PixelRGB<uint8> > final_result =
         orthoproject(output_dem, drg_georef,
-                     constant_view(PixelGrayA<uint8>(255,255),texture_image.cols(),texture_image.rows()),
+                     constant_view(PixelRGB<uint8>(opt.color[0],opt.color[1],opt.color[2]),
+                                   fmt.cols,fmt.rows),
                      camera_model,
                      BicubicInterpolation(), ZeroEdgeExtension());
-      DiskImageResourceGDAL rsrc(opt.output_file, final_result.format(), Vector2i(vw_settings().default_tile_size(),vw_settings().default_tile_size()) );
+      DiskImageResourceGDAL rsrc(opt.output_file, final_result.format() );
       write_georeference(rsrc, drg_georef);
       write_image(rsrc, final_result, TerminalProgressCallback("asp",""));
     } else {
-      vw_out() << "\t--> Orthoprojecting texture.\n";
-      ImageViewRef<PixelGrayA<uint8> > final_result =
-        orthoproject(output_dem, drg_georef,
-                     texture_image, camera_model,
-                     BicubicInterpolation(), ZeroEdgeExtension());
-
-      DiskImageResourceGDAL rsrc(opt.output_file, final_result.format(), Vector2i(vw_settings().default_tile_size(),vw_settings().default_tile_size()) );
-      write_georeference(rsrc, drg_georef);
-      write_image(rsrc, final_result, TerminalProgressCallback("asp",""));
+      switch(fmt.pixel_format) {
+      case VW_PIXEL_GRAY:
+      case VW_PIXEL_GRAYA:
+      default:
+        switch(fmt.channel_type) {
+        case VW_CHANNEL_UINT8: do_projection<PixelGrayA<uint8> >( opt, output_dem,
+                                                                  drg_georef,
+                                                                  camera_model ); break;
+        case VW_CHANNEL_INT16: do_projection<PixelGrayA<int16> >( opt, output_dem,
+                                                                  drg_georef,
+                                                                  camera_model ); break;
+        case VW_CHANNEL_UINT16: do_projection<PixelGrayA<uint16> >( opt, output_dem,
+                                                                    drg_georef,
+                                                                    camera_model ); break;
+        default: do_projection<PixelGrayA<float32> >( opt, output_dem,
+                                                      drg_georef,
+                                                      camera_model ); break;
+        }
+        break;
+      case VW_PIXEL_RGB:
+      case VW_PIXEL_RGBA:
+        switch(fmt.channel_type) {
+        case VW_CHANNEL_UINT8: do_projection<PixelRGBA<uint8> >( opt, output_dem,
+                                                                 drg_georef,
+                                                                 camera_model ); break;
+        case VW_CHANNEL_INT16: do_projection<PixelRGBA<int16> >( opt, output_dem,
+                                                                 drg_georef,
+                                                                 camera_model ); break;
+        case VW_CHANNEL_UINT16: do_projection<PixelRGBA<uint16> >( opt, output_dem,
+                                                                   drg_georef,
+                                                                   camera_model ); break;
+        default: do_projection<PixelRGBA<float32> >( opt, output_dem,
+                                                     drg_georef,
+                                                     camera_model ); break;
+        }
+        break;
+      }
     }
 
   } ASP_STANDARD_CATCHES;
