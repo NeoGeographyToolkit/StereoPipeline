583459746cb40cf5ac92f34af463a2b333e49d73
Trey Smith <trey.smith@nasa.gov>
2011-04-21 10:32:48 -0700

    committed last version of runReconstruct.sh, now superseded by runReconstruct.py

--- src/asp/Tools/runReconstruct.sh
+++ src/asp/Tools/runReconstruct.sh
@@ -1,22 +1,29 @@
-#!bin/sh
+#!/bin/sh
 
-#TIFFS=`ls ../data/orbit33/DRG_sub4/*.tif | head -3`
-#TIFFS=`ls ../data/orbit33/DRG_sub4/*.tif | tail -3`  
-TIFFS=`ls ../data/orbit33/DRG_sub4/*tif`
-#echo $TIFFS
+export DATA_DIR=/home/mfsmith3/a15
+RESULTS_DIR=$HOME/results/a15/sub16
+RECONSTRUCT=./reconstruct
+SUB=_sub16
+NUM_PROCESSORS=14
+
+TIFFS_FILE=/tmp/tiffs-$USER.txt
+
+#ls $DATA_DIR/DRG$SUB/*.tif | head -3 > $TIFFS_FILE
+ls $DATA_DIR/DRG$SUB/*.tif > $TIFFS_FILE
+
+TIFFS=$(cat $TIFFS_FILE)
 
 #-d denotes the DEM directory
 #-s denotes the spatial information directory used to determine the sun and satelite position
 #-e denotes the exposure time directory
 #-c denotes the configuration filename
 #-r denotes the results directory
+#-i gives the stereo pair to work on (default: work on all pairs)
+
+SETTINGS=photometry_init_${1}_settings.txt
 
 #single core implementation
-../src/asp/Tools/reconstruct -d ../data/orbit33/dem_sub4 -s  ../data/orbit33 -e ../data/orbit33 -r ../results/orbit33_sub4 -c photometry_settings.txt $TIFFS
+# $RECONSTRUCT -d $DATA_DIR/DEM$SUB -s $DATA_DIR/meta -e $DATA_DIR/meta -r $RESULTS_DIR -c $SETTINGS $TIFFS
 
 #paralel implementation
-#echo ../data/orbit33/DRG_sub4/*.tif|xargs -n 1 -I {} -P 2 ../src/asp/Tools/reconstruct -d ../data/orbit33/dem_sub4  -i {} -s ../data/orbit33 -e ../data/orbit33 -r ../results/orbit33_sub4 -c photometry_init_settings.txt $TIFFS
-#echo ../data/orbit33/DRG_sub4/*.tif|xargs -n 1 -I {} -P 2 ../src/asp/Tools/reconstruct -d ../data/orbit33/dem_sub4  -i {} -s ../data/orbit33 -e ../data/orbit33 -r ../results/orbit33_sub4 -c photometry_settings.txt $TIFFS
-
-
-
+cat $TIFFS_FILE | xargs -n 1 -t -I{} -P$NUM_PROCESSORS $RECONSTRUCT -d $DATA_DIR/DEM$SUB -s $DATA_DIR/meta -e $DATA_DIR/meta -r $RESULTS_DIR -c $SETTINGS -i {} $TIFFS
