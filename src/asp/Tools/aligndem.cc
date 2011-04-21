63303b5a2b19114af4e177a0f1ce82df36ebd1b9
Zack Moratto <zachary.m.moratto@nasa.gov>
2011-04-21 16:35:26 -0700

    Force aligndem to use ASP Common

--- src/asp/Tools/aligndem.cc
+++ src/asp/Tools/aligndem.cc
@@ -4,13 +4,6 @@
 // All Rights Reserved.
 // __END_LICENSE__
 
-
-#include <boost/program_options.hpp>
-namespace po = boost::program_options;
-
-#include <boost/filesystem.hpp>
-namespace fs = boost::filesystem;
-
 #include <vw/FileIO.h>
 #include <vw/Image.h>
 #include <vw/Cartography.h>
@@ -18,6 +11,13 @@ namespace fs = boost::filesystem;
 #include <vw/Math.h>
 #include <vw/Mosaic/ImageComposite.h>
 #include <asp/ControlNetTK/Equalization.h>
+#include <asp/Core/Common.h>
+#include <asp/Core/Macros.h>
+#include <limits>
+
+#include <boost/filesystem.hpp>
+namespace fs = boost::filesystem;
+namespace po = boost::program_options;
 
 using std::cout;
 using std::endl;
@@ -60,9 +60,9 @@ void remove_duplicates(std::vector<InterestPoint> &ip1,
                        std::vector<InterestPoint> &ip2) {
   std::vector<InterestPoint> new_ip1, new_ip2;
 
-  for (unsigned i = 0; i < ip1.size(); ++i) {
+  for (size_t i = 0; i < ip1.size(); ++i) {
     bool bad_entry = false;
-    for (unsigned j = 0; j < ip1.size(); ++j) {
+    for (size_t j = 0; j < ip1.size(); ++j) {
       if (i != j &&
           ((ip1[i].x == ip1[j].x && ip1[i].y == ip1[j].y) ||
            (ip2[i].x == ip2[j].x && ip2[i].y == ip2[j].y)) ) {
@@ -157,148 +157,161 @@ void match_orthoimages( string const& left_image_name,
 
 }
 
-int main( int argc, char *argv[] ) {
-  string dem1_name, dem2_name, ortho1_name, ortho2_name, output_prefix;
+struct Options : public asp::BaseOptions {
+  Options() : dem1_nodata(std::numeric_limits<double>::quiet_NaN()), dem2_nodata(std::numeric_limits<double>::quiet_NaN()) {}
+  // Input
+  string dem1_name, dem2_name, ortho1_name, ortho2_name;
   double dem1_nodata, dem2_nodata;
   size_t max_points;
 
-  po::options_description desc("Options");
-  desc.add_options()
-    ("help,h", "Display this help message")
-    ("max-match-points", po::value(&max_points)->default_value(800), "The max number of points that will be enforced after matching.")
-    ("default-value", po::value(&dem1_nodata), "The value of missing pixels in the first dem")
-    ("output-prefix,o", po::value(&output_prefix), "Specify the output prefix");
+  // Output
+  string output_prefix;
+};
+
+void handle_arguments( int argc, char *argv[], Options& opt ) {
+  po::options_description general_options("");
+  general_options.add_options()
+    ("max-match-points", po::value(&opt.max_points)->default_value(800), "The max number of points that will be enforced after matching.")
+    ("default-value", po::value(&opt.dem1_nodata), "The value of missing pixels in the first dem")
+    ("output-prefix,o", po::value(&opt.output_prefix), "Specify the output prefix");
+  general_options.add( asp::BaseOptionsDescription(opt) );
 
   po::options_description positional("");
   positional.add_options()
-    ("dem1", po::value(&dem1_name), "Explicitly specify the first dem")
-    ("dem2", po::value(&dem2_name), "Explicitly specify the second dem")
-    ("ortho1", po::value(&ortho1_name), "Explicitly specify the first orthoimage")
-    ("ortho2", po::value(&ortho2_name), "Explicitly specify the second orthoimage");
+    ("dem1", po::value(&opt.dem1_name), "Explicitly specify the first dem")
+    ("dem2", po::value(&opt.dem2_name), "Explicitly specify the second dem")
+    ("ortho1", po::value(&opt.ortho1_name), "Explicitly specify the first orthoimage")
+    ("ortho2", po::value(&opt.ortho2_name), "Explicitly specify the second orthoimage");
 
-  po::positional_options_description p;
-  p.add("dem1", 1);
-  p.add("ortho1", 1);
-  p.add("dem2", 1);
-  p.add("ortho2", 1);
+  po::positional_options_description positional_desc;
+  positional_desc.add("dem1", 1);
+  positional_desc.add("ortho1", 1);
+  positional_desc.add("dem2", 1);
+  positional_desc.add("ortho2", 1);
 
-  po::options_description all_options;
-  all_options.add(desc).add(positional);
+  std::ostringstream usage;
+  usage << "Usage: " << argv[0] << " <dem1> <ortho1> <dem2> <ortho2>" << endl;
 
-  po::variables_map vm;
+  po::variables_map vm =
+    asp::check_command_line( argc, argv, opt, general_options,
+                             positional, positional_desc, usage.str() );
+
+  if ( opt.dem1_name.empty() || opt.dem2_name.empty() ||
+       opt.ortho1_name.empty() || opt.ortho2_name.empty() )
+    vw_throw( ArgumentErr() << "Missing input files.\n"
+              << usage.str() << general_options );
+  if ( opt.output_prefix.empty() )
+    opt.output_prefix = change_extension(fs::path(opt.dem1_name), "").string();
+}
+
+int main( int argc, char *argv[] ) {
+
+  Options opt;
   try {
-    po::store( po::command_line_parser( argc, argv ).options(all_options).positional(p).run(), vm );
-    po::notify( vm );
-  } catch (po::error &e) {
-    cout << "Error parsing: " << e.what() << "\n\t" << desc << "\n";
-    return 1;
-  }
+    handle_arguments( argc, argv, opt );
 
-  if ( dem1_name.empty() || dem2_name.empty() ||
-       ortho1_name.empty() || ortho2_name.empty() || vm.count("help") ) {
-    cout << "Usage: " << argv[0] << " <dem1> <ortho1> <dem2> <ortho2>" << endl;
-    cout << desc << endl;
-    return 1;
-  }
+    DiskImageResourceGDAL ortho1_rsrc(opt.ortho1_name), ortho2_rsrc(opt.ortho2_name),
+      dem1_rsrc(opt.dem1_name), dem2_rsrc(opt.dem2_name);
 
-  if ( output_prefix.empty() )
-    output_prefix = change_extension(fs::path(dem1_name), "").string();
-
-  DiskImageResourceGDAL ortho1_rsrc(ortho1_name), ortho2_rsrc(ortho2_name),
-    dem1_rsrc(dem1_name), dem2_rsrc(dem2_name);
-
-  // Pull out dem nodatas
-  if ( !vm.count("default-value") && dem1_rsrc.has_nodata_read() ) {
-    dem1_nodata = dem1_rsrc.nodata_read();
-    vw_out() << "\tFound DEM1 input nodata value: " << dem1_nodata << endl;
-  }
-  if ( dem2_rsrc.has_nodata_read() ) {
-    dem2_nodata = dem2_rsrc.nodata_read();
-    vw_out() << "\tFound DEM2 input nodata value: " << dem2_nodata << endl;
-  } else {
-    vw_out() << "\tMissing nodata value for DEM2. Using DEM1 nodata value.\n";
-    dem2_nodata = dem1_nodata;
-  }
-
-  typedef DiskImageView<double> dem_type;
-  dem_type dem1_dmg(dem1_name), dem2_dmg(dem2_name);
-
-  typedef InterpolationView<EdgeExtensionView<dem_type, ConstantEdgeExtension>, BilinearInterpolation> bilinear_type;
-  typedef InterpolationView<EdgeExtensionView<dem_type, ConstantEdgeExtension>, NearestPixelInterpolation> nearest_type;
-  bilinear_type dem1_interp =
-    interpolate(dem1_dmg, BilinearInterpolation(), ConstantEdgeExtension());
-  bilinear_type dem2_interp =
-    interpolate(dem2_dmg, BilinearInterpolation(), ConstantEdgeExtension());
-  nearest_type dem1_nearest =
-    interpolate(dem1_dmg, NearestPixelInterpolation(), ConstantEdgeExtension());
-  nearest_type dem2_nearest =
-    interpolate(dem2_dmg, NearestPixelInterpolation(), ConstantEdgeExtension());
-
-  GeoReference ortho1_georef, ortho2_georef, dem1_georef, dem2_georef;
-  read_georeference(ortho1_georef, ortho1_rsrc);
-  read_georeference(ortho2_georef, ortho2_rsrc);
-  read_georeference(dem1_georef, dem1_rsrc);
-  read_georeference(dem2_georef, dem2_rsrc);
-
-  std::vector<InterestPoint> matched_ip1, matched_ip2;
-
-  match_orthoimages(ortho1_name, ortho2_name,
-                    matched_ip1, matched_ip2, max_points);
-
-  vw_out() << "\t--> Rejecting outliers using RANSAC.\n";
-  std::vector<Vector4> ransac_ip1, ransac_ip2;
-  for (size_t i = 0; i < matched_ip1.size(); i++) {
-    Vector2 point1 = ortho1_georef.pixel_to_lonlat(Vector2(matched_ip1[i].x, matched_ip1[i].y));
-    Vector2 point2 = ortho2_georef.pixel_to_lonlat(Vector2(matched_ip2[i].x, matched_ip2[i].y));
-
-    Vector2 dem_pixel1 = dem1_georef.lonlat_to_pixel(point1);
-    Vector2 dem_pixel2 = dem2_georef.lonlat_to_pixel(point2);
-
-    if ( dem1_nearest(dem_pixel1.x(),dem_pixel1.y()) != dem1_nodata &&
-         dem2_nearest(dem_pixel2.x(),dem_pixel2.y()) != dem2_nodata ) {
-
-      double alt1 = dem1_georef.datum().radius(point1.x(), point1.y()) + dem1_interp(dem_pixel1.x(), dem_pixel1.y());
-      double alt2 = dem2_georef.datum().radius(point2.x(), point2.y()) + dem2_interp(dem_pixel2.x(), dem_pixel2.y());
-
-      Vector3 xyz1 = lon_lat_radius_to_xyz(Vector3(point1.x(), point1.y(), alt1));
-      Vector3 xyz2 = lon_lat_radius_to_xyz(Vector3(point2.x(), point2.y(), alt2));
-
-      ransac_ip1.push_back(Vector4(xyz1.x(), xyz1.y(), xyz1.z(), 1));
-      ransac_ip2.push_back(Vector4(xyz2.x(), xyz2.y(), xyz2.z(), 1));
-    } else {
-      cout << "Actually dropped something.\n";
+    // Pull out dem nodatas
+    if ( opt.dem1_nodata == std::numeric_limits<double>::quiet_NaN() &&
+         dem1_rsrc.has_nodata_read() ) {
+      opt.dem1_nodata = dem1_rsrc.nodata_read();
+      vw_out() << "\tFound DEM1 input nodata value: "
+               << opt.dem1_nodata << endl;
     }
-  }
+    if ( dem2_rsrc.has_nodata_read() ) {
+      opt.dem2_nodata = dem2_rsrc.nodata_read();
+      vw_out() << "\tFound DEM2 input nodata value: "
+               << opt.dem2_nodata << endl;
+    } else {
+      vw_out() << "\tMissing nodata value for DEM2. Using DEM1 nodata value.\n";
+      opt.dem2_nodata = opt.dem1_nodata;
+    }
 
-  std::vector<int> indices;
-  Matrix<double> trans;
-  math::RandomSampleConsensus<math::AffineFittingFunctorN<3>,math::L2NormErrorMetric>
-    ransac( math::AffineFittingFunctorN<3>(), math::L2NormErrorMetric(), 10);
-  trans = ransac(ransac_ip1, ransac_ip2);
-  indices = ransac.inlier_indices(trans, ransac_ip1, ransac_ip2);
+    typedef DiskImageView<double> dem_type;
+    dem_type dem1_dmg(opt.dem1_name), dem2_dmg(opt.dem2_name);
 
-  vw_out() << "\t    * Ransac Result: " << trans << "\n";
-  vw_out() << "\t                     # inliers: " << indices.size() << "\n";
+    typedef InterpolationView<EdgeExtensionView<dem_type, ConstantEdgeExtension>, BilinearInterpolation> bilinear_type;
+    typedef InterpolationView<EdgeExtensionView<dem_type, ConstantEdgeExtension>, NearestPixelInterpolation> nearest_type;
+    bilinear_type dem1_interp =
+      interpolate(dem1_dmg, BilinearInterpolation(), ConstantEdgeExtension());
+    bilinear_type dem2_interp =
+      interpolate(dem2_dmg, BilinearInterpolation(), ConstantEdgeExtension());
+    nearest_type dem1_nearest =
+      interpolate(dem1_dmg, NearestPixelInterpolation(), ConstantEdgeExtension());
+    nearest_type dem2_nearest =
+      interpolate(dem2_dmg, NearestPixelInterpolation(), ConstantEdgeExtension());
 
-  { // Saving transform to human readable text
-    std::string filename = (fs::path(dem1_name).branch_path() / (fs::basename(fs::path(dem1_name)) + "__" + fs::basename(fs::path(dem2_name)) + "-Matrix.txt")).string();
-    std::ofstream ofile( filename.c_str() );
-    ofile << std::setprecision(15) << std::flush;
-    ofile << "# inliers: " << indices.size() << endl;
-    ofile << trans << endl;
-    ofile.close();
-  }
+    GeoReference ortho1_georef, ortho2_georef, dem1_georef, dem2_georef;
+    read_georeference(ortho1_georef, ortho1_rsrc);
+    read_georeference(ortho2_georef, ortho2_rsrc);
+    read_georeference(dem1_georef, dem1_rsrc);
+    read_georeference(dem2_georef, dem2_rsrc);
 
-  ImageViewRef<PixelMask<double> > dem1_masked(create_mask(dem1_dmg, dem1_nodata));
+    std::vector<InterestPoint> matched_ip1, matched_ip2;
 
-  ImageViewRef<Vector3> point_cloud = lon_lat_radius_to_xyz(project_point_image(dem_to_point_image(dem1_masked, dem1_georef), dem1_georef, false));
-  ImageViewRef<Vector3> point_cloud_trans = per_pixel_filter(point_cloud, HomogeneousTransformFunctor<3>(trans));
+    match_orthoimages(opt.ortho1_name, opt.ortho2_name,
+                      matched_ip1, matched_ip2, opt.max_points);
 
-  DiskImageResourceGDAL point_cloud_rsrc(output_prefix + "-PC.tif", point_cloud_trans.format(),
-                                         Vector2i(vw_settings().default_tile_size(),
-                                                  vw_settings().default_tile_size()));
-  block_write_image(point_cloud_rsrc, point_cloud_trans,
-                    TerminalProgressCallback("asp", "\t--> Transforming: "));
+    vw_out() << "\t--> Rejecting outliers using RANSAC.\n";
+    std::vector<Vector4> ransac_ip1, ransac_ip2;
+    for (size_t i = 0; i < matched_ip1.size(); i++) {
+      Vector2 point1 = ortho1_georef.pixel_to_lonlat(Vector2(matched_ip1[i].x, matched_ip1[i].y));
+      Vector2 point2 = ortho2_georef.pixel_to_lonlat(Vector2(matched_ip2[i].x, matched_ip2[i].y));
+
+      Vector2 dem_pixel1 = dem1_georef.lonlat_to_pixel(point1);
+      Vector2 dem_pixel2 = dem2_georef.lonlat_to_pixel(point2);
+
+      if ( dem1_nearest(dem_pixel1.x(),dem_pixel1.y()) != opt.dem1_nodata &&
+           dem2_nearest(dem_pixel2.x(),dem_pixel2.y()) != opt.dem2_nodata ) {
+
+        double alt1 = dem1_georef.datum().radius(point1.x(), point1.y()) + dem1_interp(dem_pixel1.x(), dem_pixel1.y());
+        double alt2 = dem2_georef.datum().radius(point2.x(), point2.y()) + dem2_interp(dem_pixel2.x(), dem_pixel2.y());
+
+        Vector3 xyz1 = lon_lat_radius_to_xyz(Vector3(point1.x(), point1.y(), alt1));
+        Vector3 xyz2 = lon_lat_radius_to_xyz(Vector3(point2.x(), point2.y(), alt2));
+
+        ransac_ip1.push_back(Vector4(xyz1.x(), xyz1.y(), xyz1.z(), 1));
+        ransac_ip2.push_back(Vector4(xyz2.x(), xyz2.y(), xyz2.z(), 1));
+      } else {
+        vw_out() << "Actually dropped something.\n";
+      }
+    }
+
+    std::vector<int> indices;
+    Matrix<double> trans;
+    math::RandomSampleConsensus<math::AffineFittingFunctorN<3>,math::L2NormErrorMetric>
+      ransac( math::AffineFittingFunctorN<3>(), math::L2NormErrorMetric(), 10);
+    trans = ransac(ransac_ip1, ransac_ip2);
+    indices = ransac.inlier_indices(trans, ransac_ip1, ransac_ip2);
+
+    vw_out() << "\t    * Ransac Result: " << trans << "\n";
+    vw_out() << "\t                     # inliers: " << indices.size() << "\n";
+
+    { // Saving transform to human readable text
+      std::string filename = (fs::path(opt.dem1_name).branch_path() / (fs::basename(fs::path(opt.dem1_name)) + "__" + fs::basename(fs::path(opt.dem2_name)) + "-Matrix.txt")).string();
+      std::ofstream ofile( filename.c_str() );
+      ofile << std::setprecision(15) << std::flush;
+      ofile << "# inliers: " << indices.size() << endl;
+      ofile << trans << endl;
+      ofile.close();
+    }
+
+    ImageViewRef<PixelMask<double> > dem1_masked(create_mask(dem1_dmg, opt.dem1_nodata));
+
+    ImageViewRef<Vector3> point_cloud =
+      lon_lat_radius_to_xyz(project_point_image(dem_to_point_image(dem1_masked, dem1_georef), dem1_georef, false));
+    ImageViewRef<Vector3> point_cloud_trans =
+      per_pixel_filter(point_cloud, HomogeneousTransformFunctor<3>(trans));
+
+    DiskImageResourceGDAL point_cloud_rsrc(opt.output_prefix + "-PC.tif",
+                                           point_cloud_trans.format(),
+                                           opt.raster_tile_size,
+                                           opt.gdal_options);
+    block_write_image(point_cloud_rsrc, point_cloud_trans,
+                      TerminalProgressCallback("asp", "\t--> Transforming: "));
+  } ASP_STANDARD_CATCHES;
 
   return 0;
 }
