07c6eecaecb096791cb59040f765fbd6f233ced9
Zack Moratto <zachary.m.moratto@nasa.gov>
2011-05-14 11:03:25 -0700

    Clean excessive comments in TimeAccumulators

--- src/asp/PhotometryTK/TimeAccumulators.h
+++ src/asp/PhotometryTK/TimeAccumulators.h
@@ -20,7 +20,7 @@ namespace pho {
 
   TimeDeltaAccumulator(double t) : m_numerator(0),
       m_denominator(0), m_curr_time(t) {}
-    
+
     template <class AValT, class RValT>
     void operator()( AValT const& drg, AValT const& albedo, RValT const& reflectance ) {
       double i = drg[0], a = albedo[0], s = drg[1], r = reflectance;
@@ -47,13 +47,9 @@ namespace pho {
     template <class AValT>
     void operator()( AValT const& drg, AValT const& albedo ) {
       double i = drg[0], s = drg[1], a = albedo[0];
-      //std::cout << "i=[" << i << "] s=[" << s << "] a=[" << a << "]\n";
       double inter = a*s;
-      //std::cout << "inter=[" << inter << "]\n";
       m_numerator += (i - m_curr_time*a)*inter;
-      //std::cout << "numerator=[" << m_numerator << "]\n";
       m_denominator += inter*inter;
-      //std::cout << "denominator=[" << m_denominator << "]\n";
     }
 
     value_type value() const {
