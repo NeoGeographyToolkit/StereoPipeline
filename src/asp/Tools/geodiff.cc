20e46073727f20979f9fc40790a9a56ece1ef07f
Zack Moratto <zachary.m.moratto@nasa.gov>
2012-07-16 12:41:55 -0700

    geodiff: Dust off this utility, make work with latest VW

--- src/asp/Tools/geodiff.cc
+++ src/asp/Tools/geodiff.cc
@@ -15,93 +15,138 @@
 //  limitations under the License.
 // __END_LICENSE__
 
-
-#include <boost/program_options.hpp>
-namespace po = boost::program_options;
-
-#include <boost/filesystem.hpp>
-namespace fs = boost::filesystem;
-
 #include <vw/FileIO.h>
 #include <vw/Image.h>
 #include <vw/Cartography.h>
 #include <vw/Math.h>
 
-using std::cout;
 using std::endl;
 using std::string;
 
 using namespace vw;
 using namespace vw::cartography;
 
-int main( int argc, char *argv[] ) {
-  string dem1_name, dem2_name, output_prefix;
-  double default_value;
+#include <asp/Core/Macros.h>
+#include <asp/Core/Common.h>
+namespace po = boost::program_options;
+namespace fs = boost::filesystem;
 
-  po::options_description desc("Options");
-  desc.add_options()
-    ("help,h", "Display this help message")
-    ("default-value", po::value(&default_value), "The value of missing pixels in the first dem")
-    ("output-prefix,o", po::value(&output_prefix), "Specify the output prefix");
+// Function to convert a masked geodetic vector to a masked altitude vector
+class MGeodeticToMAltitude : public ReturnFixedType<PixelMask<double> > {
+public:
+  PixelMask<double> operator()( PixelMask<Vector3> const& v ) const {
+    if ( is_transparent( v ) ) {
+      return PixelMask<double>();
+    }
+    return PixelMask<double>( v.child()[2] );
+  }
+};
+
+struct Options : asp::BaseOptions {
+  string dem1_name, dem2_name, output_prefix;
+  double nodata_value;
+
+  bool use_float, use_absolute;
+};
+
+void handle_arguments( int argc, char *argv[], Options& opt ) {
+  po::options_description general_options("");
+  general_options.add_options()
+    ("nodata_value", po::value(&opt.nodata_value)->default_value(-32767), "The value of missing pixels in the first dem")
+    ("output-prefix,o", po::value(&opt.output_prefix), "Specify the output prefix")
+    ("float", po::bool_switch(&opt.use_float)->default_value(false), "Output using float (32 bit) instead of using doubles (64 bit).")
+    ("absolute", po::bool_switch(&opt.use_absolute)->default_value(false), "Output the absolute difference as opposed to just the difference.");
+  general_options.add( asp::BaseOptionsDescription(opt) );
 
   po::options_description positional("");
   positional.add_options()
-    ("dem1", po::value(&dem1_name), "Explicitly specify the first dem")
-    ("dem2", po::value(&dem2_name), "Explicitly specify the second dem");
+    ("dem1", po::value(&opt.dem1_name), "Explicitly specify the first dem")
+    ("dem2", po::value(&opt.dem2_name), "Explicitly specify the second dem");
 
-  po::positional_options_description p;
-  p.add("dem1", 1);
-  p.add("dem2", 1);
+  po::positional_options_description positional_desc;
+  positional_desc.add("dem1", 1);
+  positional_desc.add("dem2", 1);
 
-  po::options_description all_options;
-  all_options.add(desc).add(positional);
+  std::string usage("[options] <dem1> <dem2>");
+  po::variables_map vm =
+    asp::check_command_line( argc, argv, opt, general_options,
+                             positional, positional_desc, usage );
 
-  po::variables_map vm;
+  if ( opt.dem1_name.empty() || opt.dem2_name.empty() )
+    vw_throw( ArgumentErr() << "Requires <dem1> and <dem2> in order to proceed.\n\n" << usage << general_options );
+
+  if ( opt.output_prefix.empty() ) {
+    opt.output_prefix =
+      fs::basename(opt.dem1_name) + "__" + fs::basename(opt.dem2_name);
+  }
+}
+
+int main( int argc, char *argv[] ) {
+
+  Options opt;
   try {
-    po::store( po::command_line_parser( argc, argv ).options(desc).positional(p).run(), vm );
-    po::notify( vm );
-  } catch (po::error const& e) {
-    cout << "Error parsing: " << e.what() << "\n\t" << desc << "\n";
-    return 1;
-  }
+    handle_arguments( argc, argv, opt );
 
-  if ( dem1_name.empty() || dem2_name.empty() || vm.count("help" ) ) {
-    cout << "Usage: " << argv[0] << " <dem1> <dem2> " << endl;
-    cout << desc << endl;
-    return 1;
-  }
+    DiskImageResourceGDAL dem1_rsrc(opt.dem1_name), dem2_rsrc(opt.dem2_name);
+    double dem1_nodata = opt.nodata_value, dem2_nodata = opt.nodata_value;
+    if ( dem1_rsrc.has_nodata_read() ) {
+      dem1_nodata = dem1_rsrc.nodata_read();
+      vw_out() << "\tFound input nodata value for DEM 1: " << dem1_nodata << endl;
+    }
+    if ( dem2_rsrc.has_nodata_read() ) {
+      dem2_nodata = dem2_rsrc.nodata_read();
+      vw_out() << "\tFound input nodata value for DEM 2: " << dem2_nodata << endl;
+    }
 
-  if ( output_prefix.empty() ) {
-    fs::path dem1_path(dem1_name), dem2_path(dem2_name);
-    output_prefix = (dem1_path.branch_path() / (fs::basename(dem1_path) + "__" + fs::basename(dem2_path))).string();
-  }
+    DiskImageView<double> dem1_dmg(dem1_rsrc), dem2_dmg(dem2_rsrc);
 
-  DiskImageResourceGDAL dem1_rsrc(dem1_name), dem2_rsrc(dem2_name);
-  if ( !vm.count("default-value") && dem1_rsrc.has_nodata_read() ) {
-    default_value = dem1_rsrc.nodata_read();
-    vw_out() << "\tFound input nodata value: " << default_value << endl;
-  }
-  DiskImageView<double> dem1_dmg(dem1_name), dem2_dmg(dem2_name);
+    GeoReference dem1_georef, dem2_georef;
+    read_georeference(dem1_georef, dem1_rsrc);
+    read_georeference(dem2_georef, dem2_rsrc);
 
-  GeoReference dem1_georef, dem2_georef;
-  read_georeference(dem1_georef, dem1_rsrc);
-  read_georeference(dem2_georef, dem2_rsrc);
+    // Transform DEM 2 into the same perspective as DEM 1. However, we
+    // don't support datum changes!
+    if ( dem1_georef.datum().proj4_str() !=
+         dem2_georef.datum().proj4_str() ) {
+      vw_throw( NoImplErr() << "GeoDiff can't difference DEMs which are on different datums.\n" );
+    }
 
-  // dem2_reproj is calculated in the event that two dem's datums are 
-  // different (for example, USGS uses 3396190m for the radius of Mars, 
-  // while we use 3396000m)
-  ImageViewRef<double> dem2_reproj = select_channel(reproject_point_image(dem_to_point_image(dem2_dmg, dem2_georef), dem2_georef, dem1_georef), 2);
-  ImageViewRef<double> dem2_trans = crop(geo_transform(dem2_reproj, dem2_georef, dem1_georef), 0, 0, dem1_dmg.cols(), dem1_dmg.rows());
+    ImageViewRef<PixelMask<double> > dem2_trans =
+      crop(geo_transform( per_pixel_filter(dem_to_geodetic( create_mask( dem2_dmg, dem2_nodata ), dem2_georef),
+                                           MGeodeticToMAltitude()),
+                          dem2_georef, dem1_georef,
+                          ValueEdgeExtension<PixelMask<double> >(PixelMask<double>()) ),
+           bounding_box( dem1_dmg ) );
 
-  ImageViewRef<PixelMask<double> > diff_masked = copy_mask(dem1_dmg - dem2_trans, create_mask(dem1_dmg, default_value));
-  ImageViewRef<double> diff = apply_mask(diff_masked, default_value);
+    ImageViewRef<double> difference;
+    if ( opt.use_absolute ) {
+      difference =
+        apply_mask(create_mask(dem1_dmg, dem1_nodata) - dem2_trans,
+                   opt.nodata_value );
+    } else {
+      difference =
+        apply_mask(abs(create_mask(dem1_dmg, dem1_nodata) - dem2_trans),
+                   opt.nodata_value );
+    }
 
-  DiskImageResourceGDAL output_rsrc(output_prefix + "-diff.tif", diff.format(), 
-                                         Vector2i(vw_settings().default_tile_size(), 
-                                                  vw_settings().default_tile_size()));
-  write_georeference(output_rsrc, dem1_georef);
-  block_write_image(output_rsrc, diff,
-                    TerminalProgressCallback("asp", "\t--> Differencing: "));
+    if ( opt.use_float ) {
+      ImageViewRef<float> difference_float = channel_cast<float>( difference );
+      boost::scoped_ptr<DiskImageResourceGDAL> rsrc( asp::build_gdal_rsrc( opt.output_prefix + "-diff.tif",
+                                                                           difference_float, opt ) );
+      rsrc->set_nodata_write( opt.nodata_value );
+      write_georeference( *rsrc, dem1_georef );
+      block_write_image( *rsrc, difference_float,
+                         TerminalProgressCallback("asp", "\t--> Differencing: ") );
+    } else {
+      boost::scoped_ptr<DiskImageResourceGDAL> rsrc( asp::build_gdal_rsrc( opt.output_prefix + "-diff.tif",
+                                                                           difference, opt ) );
+      rsrc->set_nodata_write( opt.nodata_value );
+      write_georeference( *rsrc, dem1_georef );
+      block_write_image( *rsrc, difference,
+                         TerminalProgressCallback("asp", "\t--> Differencing: ") );
+    }
+
+  } ASP_STANDARD_CATCHES;
 
   return 0;
 }
