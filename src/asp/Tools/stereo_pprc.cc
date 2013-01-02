a1ddb86318b2fc663a663598045424af1519f5fc
Oleg Alexandrov <oleg.alexandrov@gmail.com>
2013-01-02 10:33:18 -0800

    stereo_rfne: Undo commit submitted by mistake

--- src/asp/Tools/stereo_pprc.cc
+++ src/asp/Tools/stereo_pprc.cc
@@ -114,24 +114,14 @@ ImageViewRef< PixelMask<uint8> > mask_and_fill_holes( ImageViewRef< PixelGray<fl
 
 void stereo_preprocessing( Options& opt ) {
 
-  std::cout << "corr kernel is: " << stereo_settings().kernel << std::endl;
-  std::cout << "subpixel kernel is: " << stereo_settings().subpixel_kernel  << std::endl;
-  
   vw_out() << "\n[ " << current_posix_time_string()
            << " ] : Stage 0 --> PREPROCESSING \n";
 
   std::string pre_preproc_file_left, pre_preproc_file_right;
-//   char * proj = getenv("PROJ");
-// //   if (proj && atoi(proj)){
-//     opt.session->pre_preprocessing_hook("L1.tif", "R1.tif",
-//                                         pre_preproc_file_left,
-//                                         pre_preproc_file_right);
-//   }else{
   opt.session->pre_preprocessing_hook(opt.in_file1, opt.in_file2,
                                       pre_preproc_file_left,
                                       pre_preproc_file_right);
-  //   }
-  
+
   DiskImageView<PixelGray<float> > left_image(pre_preproc_file_left),
     right_image(pre_preproc_file_right);
 
