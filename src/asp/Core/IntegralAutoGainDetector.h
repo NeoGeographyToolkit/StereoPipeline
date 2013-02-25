NeoGeographyToolkit/StereoPipeline

commit bdf4a7b1757400ee59a593419970f66d4371ac42

Core: Remove some unneeded allocations

src/asp/Core/IntegralAutoGainDetector.h
@@ -42,9 +42,13 @@ namespace asp {
       typedef ip::ImageInterestData<ImageT,ip::OBALoGInterestOperator> DataT;
       Timer total("\t\tTotal elapsed time", DebugMessage, "interest_point");
 
-      // Rendering own standard copy of the image as the passed in
-      // view is just a cropview
-      ImageT original_image = image.impl();
+      ViewT original_image = image.impl();
+
+      // The ImageInterestData structure doesn't really apply to
+      // OBALoG. We don't need access to the original image after
+      // we've made the integral image. To avoid excessive copying,
+      // we're making an empty image to feed that structure.
+      ImageT empty_image;
 
       // Producing Integral Image
       ImageT integral_image;
@@ -56,8 +60,8 @@ namespace asp {
 
       // Creating Scales
       std::deque<DataT> interest_data;
-      interest_data.push_back( DataT(original_image, integral_image) );
-      interest_data.push_back( DataT(original_image, integral_image) );
+      interest_data.push_back( DataT(empty_image, integral_image) );
+      interest_data.push_back( DataT(empty_image, integral_image) );
 
       // Priming scales
       vw::ip::InterestPointList new_points;
@@ -74,7 +78,7 @@ namespace asp {
       // Finally processing scales
       for ( int scale = 2; scale < m_scales; scale++ ) {
 
-        interest_data.push_back( DataT(original_image, integral_image) );
+        interest_data.push_back( DataT(empty_image, integral_image) );
         {
           vw_out(DebugMessage, "interest_point") << "\tScale " << scale << " ... ";
           Timer t("done, elapsed time", DebugMessage, "interest_point");

commit 2959b8475e5e7754d97589f0217e47af852d5384

Core: Fix mistake introduced in bdf4a7b
I removed one too many allocations! This copy of the input image is
actually very important as we were dealing with a lazy view up to this
point. Integral Image operation is a per pixel heavy operation that
access each pixel twice. It was causing havok on the cache system
prior to this commit and after bdf4a7b.

src/asp/Core/IntegralAutoGainDetector.h
@@ -42,7 +42,9 @@ namespace asp {
       typedef ip::ImageInterestData<ImageT,ip::OBALoGInterestOperator> DataT;
       Timer total("\t\tTotal elapsed time", DebugMessage, "interest_point");
 
-      ViewT original_image = image.impl();
+      // The input image is a lazy view. We'll rasterize so we're not
+      // hitting the cache all of the image.
+      ImageT original_image = image.impl();
 
       // The ImageInterestData structure doesn't really apply to
       // OBALoG. We don't need access to the original image after