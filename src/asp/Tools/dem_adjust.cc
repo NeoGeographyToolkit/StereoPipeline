0b4556b6b2a3ad1b759fc98d63841db7613ba0d8
Oleg Alexandrov <oleg.alexandrov@gmail.com>
2013-03-01 15:33:00 -0800

    dem_adjust: Speed up by reading geoid in memory, other minor

--- src/asp/Tools/dem_adjust.cc
+++ src/asp/Tools/dem_adjust.cc
@@ -34,7 +34,7 @@ using namespace vw;
 using namespace vw::cartography;
 
 template <class ImageT>
-class AdjustDemView : public ImageViewBase<AdjustDemView<ImageT> >
+class DemAdjustView : public ImageViewBase<DemAdjustView<ImageT> >
 {
   ImageT m_img;
   GeoReference const& m_georef;
@@ -46,9 +46,9 @@ public:
 
   typedef double pixel_type;
   typedef double result_type;
-  typedef ProceduralPixelAccessor<AdjustDemView> pixel_accessor;
+  typedef ProceduralPixelAccessor<DemAdjustView> pixel_accessor;
 
-  AdjustDemView(ImageT const& img, GeoReference const& georef,
+  DemAdjustView(ImageT const& img, GeoReference const& georef,
                 ImageViewRef<PixelMask<double> > const& delta,
                 GeoReference const& delta_georef, double nodata_val):
     m_img(img), m_georef(georef),
@@ -62,30 +62,32 @@ public:
   inline pixel_accessor origin() const { return pixel_accessor(*this); }
 
   inline result_type operator()( size_t col, size_t row, size_t p=0 ) const {
-    
+
     if ( m_img(col, row, p) == m_nodata_val ) return m_nodata_val;
-    
+
     Vector2 lonlat = m_georef.pixel_to_lonlat(Vector2(col, row));
 
     // Wrap the lonlat until it is in the [0, 360) x [-90, 90) box for
     // interpolation.
+    // lonlat[0] = -121; lonlat[1] = 37; // For testing (see example below)
     while( lonlat[0] <   0.0  ) lonlat[0] += 360.0;
     while( lonlat[0] >= 360.0 ) lonlat[0] -= 360.0;
     while( lonlat[1] <  -90.0 ) lonlat[1] += 180.0;
     while( lonlat[1] >=  90.0 ) lonlat[1] -= 180.0;
 
-    result_type delta_height = 0.0;
-    Vector2 pix = m_delta_georef.lonlat_to_pixel(lonlat);
-    PixelMask<double> interp_val = m_delta(pix[0], pix[1]);
-    if (is_valid(interp_val)) delta_height = interp_val;
-    result_type height_above_ellipsoid = m_img(col, row, p);
-    result_type height_above_geoid = height_above_ellipsoid - delta_height;
+    result_type       delta_height           = 0.0;
+    Vector2           pix                    = m_delta_georef.lonlat_to_pixel(lonlat);
+    PixelMask<double> interp_val             = m_delta(pix[0], pix[1]);
+    if (is_valid(interp_val)) delta_height   = interp_val;
+    result_type       height_above_ellipsoid = m_img(col, row, p);
+    // See the note in the main program about the formula below
+    result_type height_above_geoid           = height_above_ellipsoid - delta_height;
 
     return height_above_geoid;
   }
-  
+
   /// \cond INTERNAL
-  typedef AdjustDemView<typename ImageT::prerasterize_type> prerasterize_type;
+  typedef DemAdjustView<typename ImageT::prerasterize_type> prerasterize_type;
   inline prerasterize_type prerasterize( BBox2i const& bbox ) const {
     return prerasterize_type( m_img.prerasterize(bbox), m_georef,
                               m_delta, m_delta_georef, m_nodata_val );
@@ -95,31 +97,31 @@ public:
 };
 
 template <class ImageT>
-AdjustDemView<ImageT>
-adjust_dem( ImageViewBase<ImageT> const& img, GeoReference const& georef,
+DemAdjustView<ImageT>
+dem_adjust( ImageViewBase<ImageT> const& img, GeoReference const& georef,
             ImageViewRef<PixelMask<double> > const& delta,
             GeoReference const& delta_georef, double nodata_val) {
-  return AdjustDemView<ImageT>( img.impl(), georef, delta, delta_georef, nodata_val );
+  return DemAdjustView<ImageT>( img.impl(), georef, delta, delta_georef, nodata_val );
 }
 
 struct Options : asp::BaseOptions {
   string geoid, delta_file, dem_name, output_prefix;
   double nodata_value;
-  bool use_float;
+  bool use_double;
 };
 
 void handle_arguments( int argc, char *argv[], Options& opt ){
-  
+
   po::options_description general_options("");
   general_options.add_options()
     ("nodata_value", po::value(&opt.nodata_value)->default_value(-32767),
      "The value of no-data pixels, unless specified in the DEM.")
     ("geoid", po::value(&opt.geoid)->default_value("EGM96"), "Choose a geoid [EGM96, NAVD88].")
     ("output-prefix,o", po::value(&opt.output_prefix), "Specify the output prefix.")
-    ("float", po::bool_switch(&opt.use_float)->default_value(false), "Output using float (32 bit) instead of using doubles (64 bit).");
+    ("double", po::bool_switch(&opt.use_double)->default_value(false)->implicit_value(true), "Output using double (64 bit) instead of float (32 bit).");
 
   general_options.add( asp::BaseOptionsDescription(opt) );
-  
+
   po::options_description positional("");
   positional.add_options()
     ("dem", po::value(&opt.dem_name), "Explicitly specify the DEM.");
@@ -152,10 +154,31 @@ void handle_arguments( int argc, char *argv[], Options& opt ){
   }
 }
 
+// Given a DEM, with each height value relative to the datum
+// ellipsoid, convert the heights to be relative to the geoid.
+
+// From: http://earth-info.nga.mil/GandG/wgs84/gravitymod/egm96/intpthel.html
+// Geoid heights can be used to convert between orthometric
+// heights (approximately mean sea level) and ellipsoid heights
+// according to the formula:
+// h   =   H   +   N
+// where,
+// h = WGS 84 Ellipsoid height
+// H = Orthometric height
+// N = EGM96 Geoid height
+// Therefore: H = h - N.
+
+// We support two geoids: EGM96 and  NAVD88.
+// Online tools for verification:
+// EGM96:  http://earth-info.nga.mil/GandG/wgs84/gravitymod/egm96/intpt.html
+// NAVD88: http://www.ngs.noaa.gov/cgi-bin/GEOID_STUFF/geoid09_prompt1.prl
+//
+// Example, at lat = 37 and lon = -121 (Basalt Hills, CA), we have
+// EGM96  geoid height = -32.69
+// NAVD88 geoid height = -32.931
+
 int main( int argc, char *argv[] ) {
 
-  // Adjust the DEM values so that they are relative to the geoid
-  // rather than to the ellipsoid.
   Options opt;
   try {
     handle_arguments( argc, argv, opt );
@@ -171,27 +194,37 @@ int main( int argc, char *argv[] ) {
     GeoReference dem_georef;
     read_georeference(dem_georef, dem_rsrc);
 
-    // Read the DEM geoid containing the adjustments
-    double delta_nodata_val = opt.nodata_value;
+    // Read the geoid containing the adjustments. Read it in memory
+    // entirely to dramatically speed up the computations.
+    double delta_nodata_val = std::numeric_limits<float>::quiet_NaN();
     DiskImageResourceGDAL delta_rsrc(opt.delta_file);
     if ( delta_rsrc.has_nodata_read() ) {
       delta_nodata_val = delta_rsrc.nodata_read();
-      vw_out() << "\tFound input nodata value for " << opt.delta_file << ": " << delta_nodata_val << endl;
     }
-    DiskImageView<double> delta_img(delta_rsrc);
+    vw_out() << "\tAdjusting the DEM using the geoid: " << opt.delta_file << endl;
+    ImageView<float> delta_img = DiskImageView<float>(delta_rsrc);
     GeoReference delta_georef;
     read_georeference(delta_georef, delta_rsrc);
     ImageViewRef<PixelMask<double> > delta
-      = interpolate(create_mask( delta_img, delta_nodata_val ),
+      = interpolate(create_mask( pixel_cast<double>(delta_img), delta_nodata_val ),
                     BicubicInterpolation(), ZeroEdgeExtension());
 
-    ImageViewRef<double> adj_dem = adjust_dem(dem_img, dem_georef,
+    ImageViewRef<double> adj_dem = dem_adjust(dem_img, dem_georef,
                                               delta, delta_georef, nodata_val);
-    
+
     std::string adj_dem_file = opt.output_prefix + "-adj.tif";
     vw_out() << "Writing adjusted DEM: " << adj_dem_file << std::endl;
-    
-    if ( opt.use_float ) {
+
+    if ( opt.use_double ) {
+      // Output as double
+      boost::scoped_ptr<DiskImageResourceGDAL> rsrc( asp::build_gdal_rsrc(adj_dem_file,
+                                                                          adj_dem, opt ) );
+      rsrc->set_nodata_write( nodata_val );
+      write_georeference( *rsrc, dem_georef );
+      block_write_image( *rsrc, adj_dem,
+                         TerminalProgressCallback("asp", "\t--> Applying DEM adjustment: ") );
+    }else{
+      // Output as float
       ImageViewRef<float> adj_dem_float = channel_cast<float>( adj_dem );
       boost::scoped_ptr<DiskImageResourceGDAL> rsrc( asp::build_gdal_rsrc(adj_dem_file,
                                                                           adj_dem_float, opt ) );
@@ -199,16 +232,9 @@ int main( int argc, char *argv[] ) {
       write_georeference( *rsrc, dem_georef );
       block_write_image( *rsrc, adj_dem_float,
                          TerminalProgressCallback("asp", "\t--> Applying DEM adjustment: ") );
-    } else {
-      boost::scoped_ptr<DiskImageResourceGDAL> rsrc( asp::build_gdal_rsrc(adj_dem_file,
-                                                                          adj_dem, opt ) );
-      rsrc->set_nodata_write( nodata_val );
-      write_georeference( *rsrc, dem_georef );
-      block_write_image( *rsrc, adj_dem,
-                         TerminalProgressCallback("asp", "\t--> Applying DEM adjustment: ") );
     }
-    
-    
+
+
   } ASP_STANDARD_CATCHES;
 
   return 0;
