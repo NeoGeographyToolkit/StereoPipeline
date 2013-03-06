0c8c8d2582d26e9882ac519434949eb0ffc1f69d
Oleg Alexandrov <oleg.alexandrov@gmail.com>
2013-03-05 21:02:55 -0800

    dem_geoid: Support Mars, compensate for datum height difference

--- src/asp/Tools/dem_geoid.cc
+++ src/asp/Tools/dem_geoid.cc
@@ -41,6 +41,7 @@ class DemGeoidView : public ImageViewBase<DemGeoidView<ImageT> >
   ImageViewRef<PixelMask<double> > const& m_geoid;
   GeoReference const& m_geoid_georef;
   bool m_reverse_adjustment;
+  double m_correction;
   double m_nodata_val;
 
 public:
@@ -51,10 +52,11 @@ public:
 
   DemGeoidView(ImageT const& img, GeoReference const& georef,
                ImageViewRef<PixelMask<double> > const& geoid,
-               GeoReference const& geoid_georef, bool reverse_adjustment, double nodata_val):
+               GeoReference const& geoid_georef, bool reverse_adjustment, double correction, double nodata_val):
     m_img(img), m_georef(georef),
     m_geoid(geoid), m_geoid_georef(geoid_georef),
     m_reverse_adjustment(reverse_adjustment),
+    m_correction(correction),
     m_nodata_val(nodata_val){}
 
   inline int32 cols() const { return m_img.cols(); }
@@ -70,8 +72,8 @@ public:
     Vector2 lonlat = m_georef.pixel_to_lonlat(Vector2(col, row));
 
     // For testing (see the link to the reference web form belows).
-    // lonlat[0] = -121; lonlat[1] = 37; // mainland US
-    // lonlat[0] = -152; lonlat[1] = 66; // Alaska
+    //lonlat[0] = -121; lonlat[1] = 37; // mainland US
+    //lonlat[0] = -152; lonlat[1] = 66; // Alaska
 
     // Need to carefully wrap lonlat to the [0, 360) x [-90, 90) box.
     // Note that lon = 25, lat = 91 is the same as lon = 180 + 25, lat = 89
@@ -93,7 +95,7 @@ public:
     Vector2           pix                    = m_geoid_georef.lonlat_to_pixel(lonlat);
     PixelMask<double> interp_val             = m_geoid(pix[0], pix[1]);
     if (!is_valid(interp_val)) return m_nodata_val;
-    geoid_height                             = interp_val.child();
+    geoid_height                             = interp_val.child() + m_correction;
     result_type       height_above_ellipsoid = m_img(col, row, p);
     double            direction              = m_reverse_adjustment?-1:1;
     // See the note in the main program about the formula below
@@ -106,7 +108,7 @@ public:
   typedef DemGeoidView<typename ImageT::prerasterize_type> prerasterize_type;
   inline prerasterize_type prerasterize( BBox2i const& bbox ) const {
     return prerasterize_type( m_img.prerasterize(bbox), m_georef,
-                              m_geoid, m_geoid_georef, m_reverse_adjustment, m_nodata_val );
+                              m_geoid, m_geoid_georef, m_reverse_adjustment, m_correction, m_nodata_val );
   }
   template <class DestT> inline void rasterize( DestT const& dest, BBox2i const& bbox ) const {
     vw::rasterize( prerasterize(bbox), dest, bbox );
@@ -118,17 +120,24 @@ template <class ImageT>
 DemGeoidView<ImageT>
 dem_geoid( ImageViewBase<ImageT> const& img, GeoReference const& georef,
            ImageViewRef<PixelMask<double> > const& geoid,
-           GeoReference const& geoid_georef, bool reverse_adjustment, double nodata_val) {
+           GeoReference const& geoid_georef, bool reverse_adjustment, double correction, double nodata_val) {
   return DemGeoidView<ImageT>( img.impl(), georef, geoid, geoid_georef,
-                               reverse_adjustment, nodata_val );
+                               reverse_adjustment, correction, nodata_val );
 }
 
 struct Options : asp::BaseOptions {
-  string geoid, geoid_file, dem_name, output_prefix;
+  string dem_name, output_prefix;
   double nodata_value;
   bool use_double;
   bool reverse_adjustment;
 };
+
+std::string get_geoids_path(){
+  // The path to the geoids, read from GEOID_PATH macro
+#define STR_EXPAND(tok) #tok
+#define STR_QUOTE(tok) STR_EXPAND(tok)
+  return STR_QUOTE(GEOID_PATH);
+}
 
 void handle_arguments( int argc, char *argv[], Options& opt ){
 
@@ -136,7 +145,6 @@ void handle_arguments( int argc, char *argv[], Options& opt ){
   general_options.add_options()
     ("nodata_value", po::value(&opt.nodata_value)->default_value(-32767),
      "The value of no-data pixels, unless specified in the DEM.")
-    ("geoid", po::value(&opt.geoid)->default_value("EGM96"), "The geoid to use [EGM96, NAVD88].")
     ("output-prefix,o", po::value(&opt.output_prefix), "Specify the output prefix.")
     ("double", po::bool_switch(&opt.use_double)->default_value(false)->implicit_value(true),
      "Output using double precision (64 bit) instead of float (32 bit).")
@@ -157,19 +165,6 @@ void handle_arguments( int argc, char *argv[], Options& opt ){
   po::variables_map vm =
     asp::check_command_line( argc, argv, opt, general_options, general_options,
                              positional, positional_desc, usage );
-
-  // The geoid containing the adjustments
-#define STR_EXPAND(tok) #tok
-#define STR_QUOTE(tok) STR_EXPAND(tok)
-  std::string geoid_path = STR_QUOTE(GEOID_PATH);
-  if (opt.geoid == "EGM96"){
-    opt.geoid_file = geoid_path + "/" + "egm96-5.tif";
-  }else if(opt.geoid == "NAVD88"){
-    opt.geoid_file = geoid_path + "/" + "NAVD88.tif";
-  }else{
-    vw_throw( ArgumentErr() << "Unknown geoid: " << opt.geoid << ".\n\n"
-              << usage << general_options );
-  }
 
   if ( opt.dem_name.empty() )
     vw_throw( ArgumentErr() << "Requires <dem> in order to proceed.\n\n"
@@ -194,17 +189,19 @@ void handle_arguments( int argc, char *argv[], Options& opt ){
 // N = EGM96 Geoid height
 // Therefore: H = h - N.
 
-// We support two geoids: EGM96 and  NAVD88.
+// We support three geoids: EGM96, NAVD88 (Earth) and MOLA MEGDR (Mars).
 // Online tools for verification:
 // EGM96:  http://earth-info.nga.mil/GandG/wgs84/gravitymod/egm96/intpt.html
 // NAVD88: http://www.ngs.noaa.gov/cgi-bin/GEOID_STUFF/geoid09_prompt1.prl
-//
-// Example, at lat = 37 and lon = -121 (Basalt Hills, CA), we have
-// EGM96  geoid height = -32.69
-// NAVD88 geoid height = -32.931
 
-// To do: Do another test with the web form for Alaska for NAVD88!
-// To do: Add Hawaii, etc.
+// Examples: At lat = 37 and lon = -121 (Basalt Hills, CA), we have
+// EGM96  geoid height = -32.69 m
+// NAVD88 geoid height = -32.931 m
+// At lat = 66 and lon = -152 (Alaska):
+// EGM96 geoid height  = 9.51 m
+// NAVD88 geoid height = 8.255 m
+
+// MOLA MEGDR: http://geo.pds.nasa.gov/missions/mgs/megdr.html
 
 int main( int argc, char *argv[] ) {
 
@@ -230,33 +227,44 @@ int main( int argc, char *argv[] ) {
     if (!has_georef)
       vw_throw( ArgumentErr() << "Missing georeference for DEM: " << opt.dem_name << "\n" );
 
+    std::string datum_name = dem_georef.datum().name(), geoid_file;
+    if ( datum_name == "WGS_1984" ){
+      geoid_file = "egm96-5.tif";
+    }else if (datum_name == "North_American_Datum_1983"){
+      geoid_file = "navd88.tif";
+    }else if (datum_name == "D_MARS"){
+      geoid_file = "mola_areoid.tif";
+    }else{
+      vw_throw( ArgumentErr() << "Cannot apply geoid adjustment to DEM relative to datum: "
+                << datum_name << "\n");
+    }
+    geoid_file = get_geoids_path() + "/" + geoid_file;
+    vw_out() << "Adjusting the DEM using the geoid: " << geoid_file << endl;
+
     // Read the geoid containing the adjustments. Read it in memory
     // entirely to dramatically speed up the computations.
     double geoid_nodata_val = std::numeric_limits<float>::quiet_NaN();
-    DiskImageResourceGDAL geoid_rsrc(opt.geoid_file);
+    DiskImageResourceGDAL geoid_rsrc(geoid_file);
     if ( geoid_rsrc.has_nodata_read() ) {
       geoid_nodata_val = geoid_rsrc.nodata_read();
     }
-    vw_out() << "\tAdjusting the DEM using the geoid: " << opt.geoid_file << endl;
     ImageView<float> geoid_img = DiskImageView<float>(geoid_rsrc);
     GeoReference geoid_georef;
     read_georeference(geoid_georef, geoid_rsrc);
 
-    if ( dem_georef.datum().name() != geoid_georef.datum().name() ){
-      vw_throw( ArgumentErr() << "Datum mismatch."
-                << "\nDEM   datum: " << dem_georef.datum().name()
-                << "\nGeoid datum: " << geoid_georef.datum().name()
-                << "\n"
-                );
+    // Need to apply an extra correction if the datum radius of the geoid is different
+    // than the datum radius of the DEM to correct.
+    double correction = geoid_georef.datum().semi_major_axis() - dem_georef.datum().semi_major_axis();
+    if (correction != 0){
+      vw_out() << "\tWill compensate the fact that the input DEM and geoid datums axis lengths differ.\n";
     }
 
     ImageViewRef<PixelMask<double> > geoid
       = interpolate(create_mask( pixel_cast<double>(geoid_img), geoid_nodata_val ),
                     BicubicInterpolation(), ZeroEdgeExtension());
 
-    ImageViewRef<double> adj_dem = dem_geoid(dem_img, dem_georef,
-                                             geoid, geoid_georef,
-                                             reverse_adjustment, dem_nodata_val);
+    ImageViewRef<double> adj_dem = dem_geoid(dem_img, dem_georef, geoid, geoid_georef,
+                                             reverse_adjustment, correction, dem_nodata_val);
 
     std::string adj_dem_file = opt.output_prefix + "-adj.tif";
     vw_out() << "Writing adjusted DEM: " << adj_dem_file << std::endl;
