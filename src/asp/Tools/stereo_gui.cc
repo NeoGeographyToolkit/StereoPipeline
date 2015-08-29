8e731400fe9c8c94131a81fe9a43cdf07b1f504e
Oleg Alexandrov <oleg.alexandrov@gmail.com>
2015-08-28 17:49:00 -0700

    More fixes to stereo_gui parsing

--- src/asp/Tools/stereo_gui.cc
+++ src/asp/Tools/stereo_gui.cc
@@ -44,6 +44,45 @@ namespace vw {
   template<> struct PixelFormatID<Vector3>   { static const PixelFormatEnum value = VW_PIXEL_GENERIC_3_CHANNEL; };
 }
 
+namespace asp{
+
+  // Parse the input command line arguments. We'll use this function
+  // only when we failed to parse the arguments under the assumption
+  // that the tool was being invoked with the stereo interface. Hence
+  // we'll fallback to this when the tool is used as an image viewer.
+  void handle_arguments( int argc, char *argv[], Options& opt,
+                         std::vector<string> & input_files){
+
+    po::options_description general_options("");
+    general_options.add(GUIDescription());
+
+    po::options_description positional_options("");
+    po::positional_options_description positional_desc;
+    positional_options.add_options()
+      ("input-files", po::value< std::vector<std::string> >(), "Input files");
+    positional_desc.add("input-files", -1);
+
+    std::string usage = "[options] <images> [<cameras>] <output_file_prefix> [DEM]";
+    bool allow_unregistered = false;
+    std::vector<std::string> unregistered;
+    po::variables_map vm = asp::check_command_line(argc, argv, opt, general_options,
+                                                   general_options, positional_options,
+                                                   positional_desc, usage,
+                                                   allow_unregistered, unregistered);
+
+    // Add the options to the usage
+    std::ostringstream os;
+    os << usage << general_options;
+    usage = os.str();
+
+    // Store the files
+    if (vm.count("input-files") == 0)
+      vw_throw(ArgumentErr() << "Missing input arguments.\n" << usage );
+    input_files = vm["input-files"].as< std::vector<std::string> >();
+  }
+
+}
+
 int main(int argc, char* argv[]) {
 
   try {
@@ -56,7 +95,7 @@ int main(int argc, char* argv[]) {
     std::vector<std::string> images;
     try {
       // See if we passed all the correct options for stereo
-      asp::parse_multiview(argc, argv, GUIDescription(),
+      asp::parse_multiview(argc, argv, asp::GUIDescription(),
                            verbose, output_prefix, opt_vec);
       // Extract the images from the options, not the cameras though.
       for (size_t i = 0; i < opt_vec.size(); i++) {
@@ -65,21 +104,32 @@ int main(int argc, char* argv[]) {
       }
 
     }catch (std::exception& e){
-      // The tool was invoked as an image viewer. Just read in any files
-      // that look like images.
-      output_prefix = ""; // mark that we did not find valid stereo options
-      for (int i = 1; i < argc; i++) {
-        std::string image = argv[i];
-        bool is_image = true;
-        try { DiskImageView<float> tmp(image); }
-        catch(...){
-          if (!image.empty() && image[0] != '-') {
-            // vw_out() << "Not a valid image: " << image << "\n";
+
+      // The tool was not invoked correctly using the stereo interface. Perhaps the
+      // user only wants to see images?
+      try {
+
+        output_prefix = ""; // mark that we did not parse correctly stereo options.
+        std::vector<std::string> all_images;
+        opt_vec.resize(1);
+        handle_arguments(argc, argv, opt_vec[0], all_images);
+
+        for (size_t i = 0; i < all_images.size(); i++) {
+          std::string image = all_images[i];
+          bool is_image = true;
+          try { DiskImageView<float> tmp(image); }
+          catch(...){
+            if (!image.empty() && image[0] != '-') {
+              vw_out() << "Not a valid image: " << image << "\n";
+            }
+            is_image = false;
           }
-          is_image = false;
+          if (is_image)
+            images.push_back(image);
         }
-        if (is_image)
-          images.push_back(image);
+      }catch (std::exception& e){
+        vw_throw(ArgumentErr() << "Use either the stereo or the image viewer interface. "
+                 << e.what() << "\n");
       }
 
       // Presumably the tool was invoked with no options. Just print the help message.
