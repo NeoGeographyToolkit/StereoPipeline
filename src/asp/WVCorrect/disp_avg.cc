2ed2b751d3529be342cf186ddd8b03215db2eccf
Oleg Alexandrov <oleg.alexandrov@gmail.com>
2020-09-29 10:24:45 -0700

    disp_avg: Can remove outliers

--- src/asp/WVCorrect/disp_avg.cc
+++ src/asp/WVCorrect/disp_avg.cc
@@ -30,104 +30,145 @@
 
 using namespace vw;
 
+namespace po = boost::program_options;
+
 // Average the rows in a given disparity image. Save them to disk as
 // two text files (x and y values), with as many entries as there
 // were columns in the disparity.
 
+struct Options : vw::cartography::GdalWriteOptions {
+  std::string disparity, dx, dy;
+  int beg_row, end_row;
+  Vector2     remove_outliers_params;
+};
+
+void handle_arguments(int argc, char *argv[], Options& opt) {
+  po::options_description general_options("");
+  general_options.add_options()
+    ("remove-outliers-params", po::value(&opt.remove_outliers_params)->default_value(Vector2(95.0, 3.0), "pct factor"))
+    ("beg-row",  po::value(&opt.beg_row)->default_value(-1), "Start averaging the disparity from this row.")
+    ("end-row",  po::value(&opt.end_row)->default_value(-1), "Stop the disparity before this row.");
+  
+  general_options.add(vw::cartography::GdalWriteOptionsDescription(opt));
+  
+  po::options_description positional("");
+  positional.add_options()
+    ("disparity", po::value(&opt.disparity))
+    ("dx", po::value(&opt.dx))
+    ("dy", po::value(&opt.dy));
+  
+  po::positional_options_description positional_desc;
+  positional_desc.add("disparity", 1);
+  positional_desc.add("dx", 1);
+  positional_desc.add("dy", 1);
+  
+  std::string usage("[options] <disparity> <dx.txt> <dy.txt>");
+  bool allow_unregistered = false;
+  std::vector<std::string> unregistered;
+  po::variables_map vm =
+    asp::check_command_line(argc, argv, opt, general_options, general_options,
+                            positional, positional_desc, usage,
+                            allow_unregistered, unregistered);
+  
+  if ( !vm.count("disparity") || !vm.count("dx") || !vm.count("dy") )
+    vw::vw_throw( vw::ArgumentErr() << "Not all inputs were specified.\n\n"
+                  << usage << general_options );
+  
+  // Create the directory in which the output image will be written.
+  vw::create_out_dir(opt.dx);
+}
+
 int main( int argc, char *argv[] ){
 
-  // TODO: Use Boost program options.
+  Options opt;
+  try {
+    handle_arguments(argc, argv, opt);
 
-  // TODO: No need for outdx.txt and outdy.txt, just save with with same prefix.
-  if (argc <= 3) {
-    vw_out() << "Usage: disp_avg disp.tif outdx.txt outdy.txt [row_start] [num_rows] [col_start] [num_cols]\n";
-    return 1;
-  }
+    DiskImageView < PixelMask<Vector2f> > D(opt.disparity);
 
-  std::string in_file = argv[1], outx = argv[2], outy = argv[3];
+    int cols = D.cols(), rows = D.rows();
 
-  std::cout << "Reading: " << in_file << std::endl;
-  DiskImageView < PixelMask<Vector2f> > D(in_file);
-
-  int cols = D.cols(), rows = D.rows();
-  std::cout << "Number of cols and rows is " << cols << ' ' << rows << std::endl;
-
-  // Handle optional row ROI arguments.
-  // - No col ROI since we want the entire column range.
-  int row_start = 0;
-  int row_stop  = rows;
-  int col_start = 0;
-  int col_stop  = cols;
-  if (argc > 4) row_start = atoi(argv[4]);
-  if (argc > 5) row_stop  = row_start + atoi(argv[5]);
-  if (argc > 6) col_start = atoi(argv[6]);
-  if (argc > 7) col_stop  = col_start + atoi(argv[7]);
-
-  TerminalProgressCallback disp_progress("asp", "\tAveraging:   ");
-  double disp_progress_mult = 1.0/double(std::max(col_stop - col_start, 1));
-
-  std::vector<double> Dx(cols, 0), Dy(cols, 0); // Always full-sized, even if crop is used.
-  for (int col = col_start; col < col_stop; col++){
-    disp_progress.report_progress((col - col_start) * disp_progress_mult);
-
-    std::vector<double> px, py;
-    for (int row = row_start; row < row_stop; row++){
-      PixelMask<Vector2f> p = D(col, row);
-      if (! is_valid(p)) continue;
-      px.push_back(p.child()[0]);
-      py.push_back(p.child()[1]);
+    // Handle optional row ROI arguments.
+    // - No col ROI since we want the entire column range.
+    int beg_row = 0;
+    int end_row  = rows;
+    int col_start = 0;
+    int col_stop  = cols;
+    if (opt.beg_row >= 0 && opt.end_row > opt.beg_row) {
+      beg_row = std::min(opt.beg_row, rows);
+      end_row = std::min(opt.end_row, rows);
     }
-    std::sort(px.begin(), px.end());
-    std::sort(py.begin(), py.end());
+    TerminalProgressCallback disp_progress("asp", "\tAveraging:   ");
+    double disp_progress_mult = 1.0/double(std::max(col_stop - col_start, 1));
 
-    double pct_factor     = 0.75;
-    double outlier_factor = 3.0;
-    double bx, ex;
-    if (!vw::math::find_outlier_brackets(px, pct_factor, outlier_factor, bx, ex))
-      continue;
-    double by, ey;
-    if (!vw::math::find_outlier_brackets(py, pct_factor, outlier_factor, by, ey))
-      continue;
+    std::vector<double> Dx(cols, 0), Dy(cols, 0);
     
-    int len = px.size();
-    int num_valid = 0;
-    Dx[col] = 0;
-    Dy[col] = 0;
-    for (int k = 0; k < len; k++){
+    for (int col = col_start; col < col_stop; col++){
+      disp_progress.report_progress((col - col_start) * disp_progress_mult);
 
-      // TODO(oalexan1): This was tested only with WV3 pan to color disparity.
-      // if (px[k] < bx || px[k] > ex || py[k] < by || py[k] > ey) continue;
+      std::vector<double> px, py;
+      for (int row = beg_row; row < end_row; row++){
+        PixelMask<Vector2f> p = D(col, row);
+        if (!is_valid(p)) continue;
+        px.push_back(p.child()[0]);
+        py.push_back(p.child()[1]);
+      }
+
+      std::sort(px.begin(), px.end());
+      std::sort(py.begin(), py.end());
       
-      num_valid++;
-      Dx[col] += px[k];
-      Dy[col] += py[k];
-    }
+      // Being too strict with outlier removal can cause trouble
+      double pct_factor     = opt.remove_outliers_params[0]/100.0;
+      double outlier_factor = opt.remove_outliers_params[1];
+      
+      double bx = 0, ex = 0, by = 0, ey = 0;
+      if (!vw::math::find_outlier_brackets(px, pct_factor, outlier_factor, bx, ex))
+        continue;
+      if (!vw::math::find_outlier_brackets(py, pct_factor, outlier_factor, by, ey))
+        continue;
 
-    if (num_valid > 0){
-      Dx[col] /= num_valid;
-      Dy[col] /= num_valid;
-    }
-  }
+      int len = px.size();
+      int num_valid = 0;
+      Dx[col] = 0;
+      Dy[col] = 0;
+      for (int k = 0; k < len; k++){
 
-  disp_progress.report_finished();
+        if (px[k] < bx || px[k] > ex || py[k] < by || py[k] > ey) continue;
+
+        num_valid++;
+        Dx[col] += px[k];
+        Dy[col] += py[k];
+      }
+      
+      if (num_valid > 0){
+        Dx[col] /= num_valid;
+        Dy[col] /= num_valid;
+      }
+    }
+    
+    disp_progress.report_finished();
   
-  // Write dx file
-  std::ofstream dx(outx.c_str());
-  dx.precision(16);
-  std::cout << "Writing: " << outx << std::endl;
-  dx << col_start << std::endl << col_stop << std::endl; // Column crop on header lines
-  for (int col = 0; col < cols; col++) 
-    dx << Dx[col] << std::endl;
-  dx.close();
+    // Write dx file
+    std::ofstream dx(opt.dx.c_str());
+    dx.precision(16);
+    std::cout << "Writing: " << opt.dx << std::endl;
+    dx << col_start << std::endl;
+    dx << col_stop  << std::endl;
+    for (int col = 0; col < cols; col++) 
+      dx << Dx[col] << std::endl;
+    dx.close();
   
-  // Write dy file
-  std::ofstream dy(outy.c_str());
-  dy.precision(16);
-  std::cout << "Writing: " << outy << std::endl;
-  dy << col_start << std::endl << col_stop << std::endl; // Column crop on header lines
-  for (int col = 0; col < cols; col++) 
-    dy << Dy[col] << std::endl;
-  dy.close();
+    // Write dy file
+    std::ofstream dy(opt.dy.c_str());
+    dy.precision(16);
+    std::cout << "Writing: " << opt.dy << std::endl;
+    dy << col_start << std::endl;
+    dy << col_stop  << std::endl;
+    for (int col = 0; col < cols; col++) 
+      dy << Dy[col] << std::endl;
+    dy.close();
+
+  } ASP_STANDARD_CATCHES;
   
   return 0;
 }
