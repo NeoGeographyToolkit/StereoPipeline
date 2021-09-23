3b8090318825b139c7e8b7ced1a2b43c9a7dbc20
Oleg Alexandrov <oleg.alexandrov@gmail.com>
2021-09-23 16:53:15 -0700

    csm_test: Make it compare any two cameras

--- src/asp/Tools/csm_test.cc
+++ src/asp/Tools/csm_test.cc
@@ -15,19 +15,27 @@
 //  limitations under the License.
 // __END_LICENSE__
 
-// Compare the CSM and ISIS models by finding for each of them the
-// camera center and ray direction at a set of sampled pixels, then by
-// projecting pixels to the ground using the ISIS camera and
-// back-projecting the resulting points into the CSM camera, then
-// doing this in reverse.
+// Tool to compare two camera models for the same image. For example,
+// compare ISIS to CSM, linescan to cam2 (for DG or PeruSat), Optical
+// bar vs pinhole (with the latter created with
+// convert_pinhole_model).
+
+// For each camera model find the camera center and ray direction at a
+// set of sampled pixels, then by projecting pixels to the ground
+// using the cam1 camera and back-projecting the resulting points into
+// the cam2 camera, then doing this in reverse.
 
 #include <asp/Core/Macros.h>
 #include <asp/Core/Common.h>
 #include <asp/Core/StereoSettings.h>
 #include <asp/Sessions/StereoSession.h>
 #include <asp/Sessions/StereoSessionFactory.h>
-
+#include <asp/Camera/RPCModel.h>
+#include <asp/Camera/LinescanPeruSatModel.h>
 #include <vw/Core/Stopwatch.h>
+#include <vw/Camera/OpticalBarModel.h>
+#include <asp/Camera/CsmModel.h>
+#include <asp/IsisIO/IsisCameraModel.h>
 
 using namespace vw;
 using namespace vw::cartography;
@@ -37,30 +45,33 @@ namespace fs = boost::filesystem;
 typedef boost::scoped_ptr<asp::StereoSession> SessionPtr;
 
 struct Options : vw::cartography::GdalWriteOptions {
-  std::string image_file, camera_file;
+  std::string image_file, cam1_file, cam2_file, session1, session2;
   int sample_rate; // use one out of these many pixels
   Options() {}
 };
 
 void handle_arguments(int argc, char *argv[], Options& opt) {
 
-  po::options_description general_options("General Options");
+  po::options_description general_options("General options");
   general_options.add_options()
     ("sample-rate",   po::value(&opt.sample_rate)->default_value(100),
-     "Use one out of these many pixels when sampling the image.");
+     "Use one out of these many pixels when sampling the image.")
+    ("image", po::value(&opt.image_file),  "Image file")
+    ("cam1",  po::value(&opt.cam1_file),   "Camera 1 file")
+    ("cam2",  po::value(&opt.cam2_file),   "Camera 2 file")
+    ("session1", po::value(&opt.session1),
+     "Session to use for camera 1 (if not provided it will be guessed).")
+    ("session2", po::value(&opt.session2),
+     "Session to use for camera 2 (if not provided it will be guessed).");
     
-  general_options.add( vw::cartography::GdalWriteOptionsDescription(opt) );
+  general_options.add(vw::cartography::GdalWriteOptionsDescription(opt));
   
   po::options_description positional("");
-  positional.add_options()
-    ("image",   po::value(&opt.image_file),   "Satellite image file")
-    ("camera",  po::value(&opt.camera_file),  "Camera file");
-  
   po::positional_options_description positional_desc;
-  positional_desc.add("image",   1);
-  positional_desc.add("camera",  1);
   
-  std::string usage("input.cub input.json [other options]");
+  std::string usage("--image <image file> --cam1 <camera 1 file> --cam2 <camera 2 file> "
+                    "[other options]");
+  
   bool allow_unregistered = false;
   std::vector<std::string> unregistered;
   po::variables_map vm =
@@ -68,11 +79,11 @@ void handle_arguments(int argc, char *argv[], Options& opt) {
                             positional, positional_desc, usage,
                             allow_unregistered, unregistered);
 
-  if (opt.image_file == "" || opt.camera_file == "")
-    vw_throw( ArgumentErr() << "Not all inputs were specified.\n" << usage << general_options );
+  if (opt.image_file == "" || opt.cam1_file == "" || opt.cam2_file == "")
+    vw_throw(ArgumentErr() << "Not all inputs were specified.\n" << usage << general_options);
 
   if (opt.sample_rate <= 0)
-    vw_throw( ArgumentErr() << "The sample rate must be positive.\n" << usage << general_options );
+    vw_throw(ArgumentErr() << "The sample rate must be positive.\n" << usage << general_options);
 }
 
 // Sort the diffs and print some stats
@@ -81,86 +92,96 @@ void print_diffs(std::string const& tag, std::vector<double> & diffs) {
 
   vw_out() << "\n";
   
-  if (diffs.empty()) 
+  if (diffs.empty()) {
     vw_out() << "Empty list of diffs for: " << tag << "\n";
-
+    return;
+  }
+  
   vw_out() << tag << "\n";
   vw_out() << "Min:    " << diffs[0] << "\n";
   vw_out() << "Median: " << diffs[diffs.size()/2] << "\n";
   vw_out() << "Max:    " << diffs.back() << "\n";
 }
 
-int main( int argc, char *argv[] ) {
+int main(int argc, char *argv[]) {
 
   Options opt;
   try {
     handle_arguments(argc, argv, opt);
 
-    // Load the isis camera
+    // Load cam1
     std::string out_prefix;
-    std::string isis_session_name; 
-    SessionPtr isis_session(asp::StereoSessionFactory::create(isis_session_name, // will change
-                                                              opt,
-                                                              opt.image_file, opt.image_file,
-                                                              opt.image_file, opt.image_file,
-                                                              out_prefix));
-    boost::shared_ptr<vw::camera::CameraModel> isis_camera_model
-      = isis_session->camera_model(opt.image_file, opt.image_file);
+    std::string default_session1 = opt.session1; // save it before it changes
+    SessionPtr cam1_session(asp::StereoSessionFactory::create
+                               (opt.session1, // may change
+                                opt,
+                                opt.image_file, opt.image_file,
+                                opt.cam1_file, opt.cam1_file,
+                                out_prefix));
+    boost::shared_ptr<vw::camera::CameraModel> cam1_model
+      = cam1_session->camera_model(opt.image_file, opt.cam1_file);
 
+    // Auto-guess the datum
     bool use_sphere_for_datum = false;
-    vw::cartography::Datum datum = isis_session->get_datum(isis_camera_model.get(),
+    vw::cartography::Datum datum = cam1_session->get_datum(cam1_model.get(),
                                                            use_sphere_for_datum);
     std::cout << "Datum: " << datum << std::endl;
     
-    // Load the csm camera
-    std::string csm_session_name; 
-    SessionPtr csm_session(asp::StereoSessionFactory::create(csm_session_name, // will change
-                                                             opt,
-                                                             opt.image_file, opt.image_file,
-                                                             opt.camera_file, opt.camera_file,
-                                                             out_prefix));
-    boost::shared_ptr<vw::camera::CameraModel> csm_camera_model
-      = csm_session->camera_model(opt.image_file, opt.camera_file);
+    // Load cam2
+    std::string default_session2 = opt.session2; // save it before it changes
+    SessionPtr cam2_session(asp::StereoSessionFactory::create
+                           (opt.session2, // may change
+                            opt,
+                            opt.image_file, opt.image_file,
+                            opt.cam2_file, opt.cam2_file,
+                            out_prefix));
+    boost::shared_ptr<vw::camera::CameraModel> cam2_model
+      = cam2_session->camera_model(opt.image_file, opt.cam2_file);
 
+    if (opt.session1 == opt.session2 && (default_session1 == "" || default_session2 == "")) 
+      vw_throw(ArgumentErr() << "The session names for both cameras "
+               << "were guessed as: '" << opt.session1 << "'. It is suggested that they be "
+               << "explicitly specified using --session1 and --session2.\n");
+    
     // The input image
     DiskImageView<float> image(opt.image_file);
     std::cout << "Image dimensions: " << image.cols() << ' ' << image.rows() << std::endl;
-    
+
     // Iterate over the image
-    std::vector<double> ctr_diff, dir_diff, isis2csm_diff, csm2isis_diff;
+    std::vector<double> ctr_diff, dir_diff, cam1_to_cam2_diff, cam2_to_cam1_diff;
     for (int col = 0; col < image.cols(); col += opt.sample_rate) {
       for (int row = 0; row < image.rows(); row += opt.sample_rate) {
 
         Vector2 image_pix(col, row);
-        Vector3 csm_ctr = csm_camera_model->camera_center(image_pix);
-        Vector3 isis_ctr = isis_camera_model->camera_center(image_pix);
-        ctr_diff.push_back(norm_2(isis_ctr - csm_ctr));
+        Vector3 cam1_ctr = cam1_model->camera_center(image_pix);
+        Vector3 cam2_ctr = cam2_model->camera_center(image_pix);
+        ctr_diff.push_back(norm_2(cam1_ctr - cam2_ctr));
 
-        Vector3 csm_dir = csm_camera_model->pixel_to_vector(image_pix);
-        Vector3 isis_dir = isis_camera_model->pixel_to_vector(image_pix);
-        dir_diff.push_back(norm_2(isis_dir - csm_dir));
+        Vector3 cam1_dir = cam1_model->pixel_to_vector(image_pix);
+        Vector3 cam2_dir = cam2_model->pixel_to_vector(image_pix);
+        dir_diff.push_back(norm_2(cam1_dir - cam2_dir));
         
-        // Shoot a ray from the ISIS camera, intersect it with the datum,
-        // and project it back into the CSM camera.
-        Vector3 xyz = vw::cartography::datum_intersection(datum, isis_ctr, isis_dir);
-        Vector2 csm_pix = csm_camera_model->point_to_pixel(xyz);
-        isis2csm_diff.push_back(norm_2(image_pix - csm_pix));
+        // Shoot a ray from the cam1 camera, intersect it with the datum,
+        // and project it back into the cam2 camera.
+        Vector3 xyz = vw::cartography::datum_intersection(datum, cam1_ctr, cam1_dir);
+        Vector2 cam2_pix = cam2_model->point_to_pixel(xyz);
+        cam1_to_cam2_diff.push_back(norm_2(image_pix - cam2_pix));
         
-        // Shoot a ray from the CSM camera, intersect it with the datum,
-        // and project it back into the ISIS camera.
-        xyz = vw::cartography::datum_intersection(datum, csm_ctr, csm_dir);
-        Vector2 isis_pix = isis_camera_model->point_to_pixel(xyz);
-        csm2isis_diff.push_back(norm_2(image_pix - isis_pix));
+        // Shoot a ray from the cam2 camera, intersect it with the datum,
+        // and project it back into the cam1 camera.
+        xyz = vw::cartography::datum_intersection(datum, cam2_ctr, cam2_dir);
+        Vector2 cam1_pix = cam1_model->point_to_pixel(xyz);
+        cam2_to_cam1_diff.push_back(norm_2(image_pix - cam1_pix));
         
       }
     }
 
     vw_out() << "Number of samples used: " << ctr_diff.size() << "\n";
-
-    print_diffs("ISIS vs CSM camera direction diff", dir_diff);
-    print_diffs("ISIS vs CSM camera center diff (meters)", ctr_diff);
-    print_diffs("ISIS to CSM pixel diff", isis2csm_diff);
-    print_diffs("CSM to ISIS pixel diff", csm2isis_diff);
+    
+    print_diffs("cam1 to cam2 camera direction diff norm", dir_diff);
+    print_diffs("cam1 to cam2 camera center diff (meters)", ctr_diff);
+    print_diffs("cam1 to cam2 pixel diff", cam1_to_cam2_diff);
+    print_diffs("cam2 to cam1 pixel diff", cam2_to_cam1_diff);
     
   } ASP_STANDARD_CATCHES;
   
