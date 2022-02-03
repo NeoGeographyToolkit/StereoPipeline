d100f2230c9bc20b85e6a8265a4e759d64ac92a6
Oleg Alexandrov <oleg.alexandrov@gmail.com>
2022-02-02 20:47:52 -0800

    Finish moving stuff to StereoSessionIsis.cc from .h and .tcc

--- src/asp/Sessions/StereoSessionIsis.cc
+++ src/asp/Sessions/StereoSessionIsis.cc
@@ -56,30 +56,15 @@
 #include <boost/math/special_functions/next.hpp> // boost::float_next
 #include <boost/shared_ptr.hpp>
 
+// Isis Headers
+#include <SpecialPixel.h>
+
 #include <algorithm>
 
 using namespace vw;
 using namespace vw::camera;
 
-//#if defined(ASP_HAVE_PKG_ISISIO) && ASP_HAVE_PKG_ISISIO == 1
-
-
-// #include <asp/IsisIO/IsisCameraModel.h>
-// #include <asp/IsisIO/DiskImageResourceIsis.h>
-// #include <asp/IsisIO/Equation.h>
-
-// // Boost
-// #include <boost/filesystem/operations.hpp>
-// #include <boost/math/special_functions/next.hpp> // boost::float_next
-// #include <boost/shared_ptr.hpp>
-// namespace fs = boost::filesystem;
-
-// #include <algorithm>
-
-// using namespace vw;
-// using namespace vw::camera;
-// using namespace asp;
-
+#if defined(ASP_HAVE_PKG_ISISIO) && ASP_HAVE_PKG_ISISIO == 1
 
 // // Allows FileIO to correctly read/write these pixel types
 // namespace vw {
@@ -87,6 +72,114 @@ using namespace vw::camera;
 
 namespace asp {
 
+//  IsisSpecialPixelFunc
+//
+/// Replace ISIS missing data values with a pixel value of your choice.
+template <class PixelT>
+class IsisSpecialPixelFunc: public vw::UnaryReturnSameType {
+  PixelT m_replacement_low;
+  PixelT m_replacement_high;
+  PixelT m_replacement_null;
+  
+  // Private
+  IsisSpecialPixelFunc() : m_replacement_low(0), m_replacement_high(0), m_replacement_null(0) {}
+  
+public:
+  IsisSpecialPixelFunc(PixelT const& pix_l, PixelT const& pix_h, PixelT const& pix_n):
+    m_replacement_low(pix_l), m_replacement_high(pix_h), m_replacement_null(pix_n) {}
+  
+  // Helper to determine special across different channel types
+  template <typename ChannelT, typename T = void>
+  struct Helper {
+    static inline bool IsSpecial( ChannelT const& arg ) { return false; }
+    static inline bool IsHighPixel( ChannelT const& arg ) { return false; }
+    static inline bool IsNull( ChannelT const& arg ) { return false; }
+  };
+  template<typename T> struct Helper<double, T> {
+    static inline bool IsSpecial( double const& arg ) {
+      return arg < Isis::VALID_MIN8;
+    }
+    static inline bool IsHighPixel( double const& arg ) {
+      return arg == Isis::HIGH_INSTR_SAT8 || arg == Isis::HIGH_REPR_SAT8;
+    }
+    static inline bool IsNull( double const& arg ) {
+      return arg == Isis::NULL8;
+    }
+  };
+  template<typename T> struct Helper<float, T> {
+    static inline bool IsSpecial( float const& arg ) {
+      return arg < Isis::VALID_MIN4;
+    }
+    static inline bool IsHighPixel( float const& arg ) {
+      return arg == Isis::HIGH_INSTR_SAT4 || arg == Isis::HIGH_REPR_SAT4;
+    }
+    static inline bool IsNull( float const& arg ) {
+      return arg == Isis::NULL4;
+    }
+  };
+  template<typename T> struct Helper<short, T>{
+    static inline bool IsSpecial( short const& arg ) {
+      return arg < Isis::VALID_MIN2;
+    }
+    static inline bool IsHighPixel( short const& arg ) {
+      return arg == Isis::HIGH_INSTR_SAT2 || arg == Isis::HIGH_REPR_SAT2;
+    }
+    static inline bool IsNull( short const& arg ) {
+      return arg == Isis::NULL2;
+    }
+  };
+  template<typename T> struct Helper<unsigned short, T> {
+    static inline bool IsSpecial( unsigned short const& arg ) {
+      return arg < Isis::VALID_MINU2 || arg > Isis::VALID_MAXU2;
+    }
+    static inline bool IsHighPixel( unsigned short const& arg ) {
+      return arg == Isis::HIGH_INSTR_SATU2 || arg == Isis::HIGH_REPR_SATU2;
+    }
+    static inline bool IsNull( unsigned short const& arg ) {
+      return arg == Isis::NULLU2;
+    }
+  };
+  template<typename T> struct Helper<unsigned char, T> {
+    static inline bool IsSpecial( unsigned char const& arg ) {
+      return arg < Isis::VALID_MIN1 || arg > Isis::VALID_MAX1;
+    }
+    static inline bool IsHighPixel( unsigned char const& arg ) {
+      return arg == Isis::HIGH_INSTR_SAT1 || arg == Isis::HIGH_REPR_SAT1;
+    }
+    static inline bool IsNull( unsigned char const& arg ) {
+      return arg == Isis::NULL1;
+    }
+  };
+
+  PixelT operator() (PixelT const& pix) const {
+    using namespace vw;
+    typedef typename CompoundChannelType<PixelT>::type channel_type;
+    typedef Helper<channel_type, void> help;
+    for (size_t n = 0; n < CompoundNumChannels<PixelT>::value; ++n) {
+      if (help::IsSpecial(compound_select_channel<const channel_type&>(pix,n))) {
+        if (help::IsHighPixel(compound_select_channel<const channel_type&>(pix,n)))
+          return m_replacement_high;
+        else if ( help::IsNull(compound_select_channel<const channel_type&>(pix,n)))
+          return m_replacement_null;
+        else
+          return m_replacement_low;
+      }
+    }
+    return pix;
+  }
+};
+
+template <class ViewT>
+vw::UnaryPerPixelView<ViewT, IsisSpecialPixelFunc<typename ViewT::pixel_type> >
+remove_isis_special_pixels(vw::ImageViewBase<ViewT> &image,
+                           typename ViewT::pixel_type r_low  = typename ViewT::pixel_type(),
+                           typename ViewT::pixel_type r_high = typename ViewT::pixel_type(),
+                           typename ViewT::pixel_type r_null = typename ViewT::pixel_type()) {
+  return vw::per_pixel_filter(image.impl(),
+                              IsisSpecialPixelFunc<typename ViewT::pixel_type>
+                              (r_low,r_high,r_null));
+}
+  
 // This actually modifies and writes the pre-processed image.
 void write_preprocessed_isis_image(vw::cartography::GdalWriteOptions const& opt,
                                     bool will_apply_user_nodata,
@@ -513,4 +606,4 @@ StereoSessionIsis::pre_pointcloud_hook(std::string const& input_file) {
   
 }
 
-//#endif  // ASP_HAVE_PKG_ISISIO
+#endif  // ASP_HAVE_PKG_ISISIO
