5c1fb5534aa5cbe7d2014e040b14a0b670a69f99
Oleg Alexandrov <oleg.alexandrov@gmail.com>
2023-07-26 14:10:02 -0700

    More jitter solve code modularization

--- src/asp/Camera/JitterSolveCostFuns.cc
+++ src/asp/Camera/JitterSolveCostFuns.cc
@@ -19,12 +19,16 @@
 // so they are stored in the Camera folder.
 
 #include <asp/Camera/JitterSolveCostFuns.h>
+#include <asp/Core/CameraTransforms.h>
+#include <asp/Core/SatSimBase.h>
+
+#include <vw/Cartography/GeoReferenceUtils.h>
 
 namespace asp {
 
 // Call to work with ceres::DynamicCostFunction.
 bool LsPixelReprojErr::operator()(double const * const * parameters, 
-                                double * residuals) const {
+                                  double * residuals) const {
 
   try {
     // Make a copy of the model, as we will update quaternion and position values
@@ -75,8 +79,123 @@ bool LsPixelReprojErr::operator()(double const * const * parameters,
     residuals[1] = g_big_pixel_value;
     return true; // accept the solution anyway
   }
-  
+
   return true;
 }
+
+// Constructor for weightedRollYawError. See the .h file for the documentation.
+weightedRollYawError::weightedRollYawError
+                  (std::vector<double>       const& positions, 
+                   std::vector<double>           const& quaternions,
+                   vw::cartography::GeoReference const& georef,
+                   int cur_pos, double rollWeight, double yawWeight,
+                   bool initial_camera_constraint): 
+                   m_rollWeight(rollWeight), m_yawWeight(yawWeight), 
+                   m_initial_camera_constraint(initial_camera_constraint) {
+
+    int num_pos = positions.size()/NUM_XYZ_PARAMS;
+    int num_quat = quaternions.size()/NUM_QUAT_PARAMS;
+    if (num_pos != num_quat)
+      vw::vw_throw(vw::ArgumentErr() 
+        << "weightedRollYawError: Expecting the same number of positions and quaternions.\n");
+    if (cur_pos < 0 || cur_pos >= num_pos)
+      vw::vw_throw(vw::ArgumentErr() 
+        << "weightedRollYawError: Expecting position index in range.\n");
+
+    // Find the nearest neighbors of the current position
+    int beg_pos = std::max(0, cur_pos - 1);
+    int end_pos = std::min(num_pos - 1, cur_pos + 1);
+    if (beg_pos >= end_pos)
+      vw::vw_throw(vw::ArgumentErr() 
+        << "weightedRollYawError: Expecting at least 2 camera positions.\n");
+
+    // Find the segment along which the cameras are located, in projected coordinates
+    // Here we mirror the logic from SatSim.cc
+    int b = beg_pos * NUM_XYZ_PARAMS;
+    int c = cur_pos * NUM_XYZ_PARAMS;
+    int e = end_pos * NUM_XYZ_PARAMS;
+    vw::Vector3 beg_pt(positions[b], positions[b+1], positions[b+2]);
+    vw::Vector3 cur_pt(positions[c], positions[c+1], positions[c+2]);
+    vw::Vector3 end_pt(positions[e], positions[e+1], positions[e+2]);
+
+    // Orbital points before the current one, the current one, and after the
+    // current one, in projected coordinates
+    vw::Vector3 beg_proj = vw::cartography::ecefToProj(georef, beg_pt);
+    vw::Vector3 cur_proj = vw::cartography::ecefToProj(georef, cur_pt);
+    vw::Vector3 end_proj = vw::cartography::ecefToProj(georef, end_pt);
+    
+    // Find satellite along and across track directions in projected coordinates
+    vw::Vector3 proj_along, proj_across;
+    asp::calcProjAlongAcross(beg_proj, end_proj, proj_along, proj_across);
+
+    // Find along and across in ECEF
+    vw::Vector3 along, across;
+    asp::calcEcefAlongAcross(georef, asp::satSimDelta(), 
+                              proj_along, proj_across, cur_proj,
+                              along, across); // outputs
+
+    // Find the z vector as perpendicular to both along and across
+    vw::Vector3 down = vw::math::cross_prod(along, across);
+    down = down / norm_2(down);
+
+    // Find the rotation matrix from satellite to world coordinates, and 90
+    // degree in-camera rotation. It is assumed, as in sat_sim, that:
+    // cam2world = sat2World * rollPitchYaw * rotXY.
+    asp::assembleCam2WorldMatrix(along, across, down, m_sat2World);
+    m_rotXY = asp::rotationXY();
+
+    // Initial camera rotation matrix, before we optimize it
+    m_initCam2World = asp::quaternionToMatrix(&quaternions[cur_pos*NUM_QUAT_PARAMS]);
+}
+
+  // See the .h file for the documentation.
+  bool weightedRollYawError::operator()(double const * const * parameters, 
+                                       double * residuals) const {
+
+    // Convert to rotation matrix. Order of quaternion is x, y, z, w.  
+    vw::Matrix3x3 cam2world = asp::quaternionToMatrix(parameters[0]);
+
+    if (m_initial_camera_constraint) {
+      // Find the new camera orientation relative to the initial camera, not
+      // relative to the satellite along-track direction. Then find the roll and
+      // yaw from it. This is experimental.
+      vw::Matrix3x3 cam2cam =  vw::math::inverse(cam2world) * m_initCam2World;
+
+      double roll, pitch, yaw;
+      rollPitchYawFromRotationMatrix(cam2cam, roll, pitch, yaw);
+
+      // Fix for roll / yaw being determined with +/- 180 degree ambiguity.
+      roll  = roll  - 180.0 * round(roll  / 180.0);
+      pitch = pitch - 180.0 * round(pitch / 180.0);
+      yaw   = yaw   - 180.0 * round(yaw   / 180.0);
+
+      // Roll, pitch, yaw in camera coordinates are pitch, roll, yaw in satellite
+      // coordinates. So adjust below accordingly.
+      // CERES is very tolerant if one of the weights used below is 0. So there is
+      // no need to use a special cost function for such cases.
+      residuals[0] = pitch * m_rollWeight; // per above, swap roll and pitch
+      residuals[1] = yaw  * m_yawWeight;
+
+      return true;
+    }
+
+    vw::Matrix3x3 rollPitchYaw  
+      = vw::math::inverse(m_sat2World) * cam2world * vw::math::inverse(m_rotXY);
+
+    double roll, pitch, yaw;
+    rollPitchYawFromRotationMatrix(rollPitchYaw, roll, pitch, yaw);
+
+    // Fix for roll / yaw being determined with +/- 180 degree ambiguity.
+    roll = roll - 180.0 * round(roll / 180.0);
+    pitch = pitch - 180.0 * round(pitch / 180.0);
+    yaw  = yaw  - 180.0 * round(yaw  / 180.0);
+
+    // CERES is very tolerant if one of the weights used below is 0. So there is
+    // no need to use a special cost function for such cases.
+    residuals[0] = roll * m_rollWeight;
+    residuals[1] = yaw  * m_yawWeight;
+
+    return true;
+  }
 
 } // end namespace asp
--- src/asp/Camera/JitterSolveCostFuns.h
+++ src/asp/Camera/JitterSolveCostFuns.h
@@ -17,13 +17,17 @@
 
 /// \file JitterSolveCostFuns.h
 
-// Cost functions used in solving for jitter. These need access to the camera models,
-// so they are stored in the Camera folder.
+// Cost functions used in solving for jitter. These need access to the camera
+// models, so they are stored in the Camera folder. The bigger functions defined
+// here are implemented in the .cc file.
 
 #ifndef __ASP_CAMERA_JITTER_SOLVE_COST_FUNS_H__
 #define __ASP_CAMERA_JITTER_SOLVE_COST_FUNS_H__
 
 #include <asp/Camera/CsmModel.h>
+
+#include <vw/Cartography/GeoReference.h>
+
 #include <usgscsm/UsgsAstroLsSensorModel.h>
 #include <usgscsm/UsgsAstroFrameSensorModel.h>
 #include <usgscsm/Utilities.h>
@@ -93,6 +97,167 @@ private:
   int m_begQuatIndex, m_endQuatIndex;
   int m_begPosIndex, m_endPosIndex;
 }; // End class LsPixelReprojErr
+
+/// A ceres cost function. The residual is the difference between the observed
+/// 3D point and the current (floating) 3D point, multiplied by given weight.
+struct weightedXyzError {
+  weightedXyzError(vw::Vector3 const& observation, double weight):
+    m_observation(observation), m_weight(weight){}
+
+  template <typename T>
+  bool operator()(const T* point, T* residuals) const {
+    for (size_t p = 0; p < m_observation.size(); p++)
+      residuals[p] = m_weight * (point[p] - m_observation[p]);
+
+    return true;
+  }
+
+  // Factory to hide the construction of the CostFunction object from
+  // the client code.
+  static ceres::CostFunction* Create(vw::Vector3 const& observation, double const& weight){
+    return (new ceres::AutoDiffCostFunction<weightedXyzError, 3, 3>
+            (new weightedXyzError(observation, weight)));
+  }
+
+  vw::Vector3 m_observation;
+  double  m_weight;
+};
+
+/// A Ceres cost function. The residual is the difference between the
+/// initial quaternion and optimized quaternion, multiplied by given weight.
+struct weightedRotationError {
+  weightedRotationError(const double * init_quat, double weight):
+    m_weight(weight) {
+
+    // Make a copy, as later the value at the pointer will change
+    m_init_quat.resize(NUM_QUAT_PARAMS);
+    for (int it = 0; it < NUM_QUAT_PARAMS; it++)
+      m_init_quat[it] = init_quat[it];
+  }
+
+  template <typename T>
+  bool operator()(const T* quat, T* residuals) const {
+    for (size_t p = 0; p < m_init_quat.size(); p++)
+      residuals[p] = m_weight * (quat[p] - m_init_quat[p]);
+
+    return true;
+  }
+
+  // Factory to hide the construction of the CostFunction object from
+  // the client code.
+  static ceres::CostFunction* Create(const double * init_quat, double weight){
+    return (new ceres::AutoDiffCostFunction<weightedRotationError,
+            NUM_QUAT_PARAMS, NUM_QUAT_PARAMS>
+            (new weightedRotationError(init_quat, weight)));
+  }
+
+  std::vector<double> m_init_quat;
+  double  m_weight;
+};
+
+/// A Ceres cost function. The residual is the difference between the
+/// initial position and optimized position, multiplied by given weight.
+struct weightedTranslationError {
+  weightedTranslationError(const double * init_position, double weight):
+    m_weight(weight) {
+
+    // Make a copy, as later the value at the pointer will change
+    m_init_position.resize(NUM_XYZ_PARAMS);
+    for (int it = 0; it < NUM_XYZ_PARAMS; it++)
+      m_init_position[it] = init_position[it];
+  }
+
+  template <typename T>
+  bool operator()(const T* position, T* residuals) const {
+    for (size_t p = 0; p < m_init_position.size(); p++)
+      residuals[p] = m_weight * (position[p] - m_init_position[p]);
+
+    return true;
+  }
+
+  // Factory to hide the construction of the CostFunction object from
+  // the client code.
+  static ceres::CostFunction* Create(const double * init_position, double weight){
+    return (new ceres::AutoDiffCostFunction
+            <weightedTranslationError, NUM_XYZ_PARAMS, NUM_XYZ_PARAMS>
+            (new weightedTranslationError(init_position, weight)));
+  }
+
+  std::vector<double> m_init_position;
+  double  m_weight;
+};
+
+/// A Ceres cost function. The residual is the weighted difference between 1 and
+/// norm of quaternion.
+struct weightedQuatNormError {
+  weightedQuatNormError(double weight):
+    m_weight(weight) {}
+
+  template <typename T>
+  bool operator()(const T* quat, T* residuals) const {
+    residuals[0] = T(0.0);
+    for (size_t p = 0; p < NUM_QUAT_PARAMS; p++)
+      residuals[0] += quat[p] * quat[p];
+
+    residuals[0] = m_weight * (residuals[0] - 1.0);
+    
+    return true;
+  }
+
+  // Factory to hide the construction of the CostFunction object from
+  // the client code.
+  static ceres::CostFunction* Create(double weight) {
+    return (new ceres::AutoDiffCostFunction<weightedQuatNormError, 1, NUM_QUAT_PARAMS>
+            (new weightedQuatNormError(weight)));
+  }
+
+  double  m_weight;
+};
+
+// A Ceres cost function. The residual is the roll and/or yaw component of the camera
+// rotation, as measured relative to the initial along-track direction. We assume
+// that all positions are along the same segment in projected coordinates, or at
+// least that the current position and its nearest neighbors are roughly on
+// such a segment. That one is used to measure the roll/yaw from. This is consistent
+// with how sat_sim creates the cameras.
+struct weightedRollYawError {
+  // See the .cc file for the implementation.  
+  weightedRollYawError(std::vector<double>       const& positions, 
+                   std::vector<double>           const& quaternions,
+                   vw::cartography::GeoReference const& georef,
+                   int cur_pos, double rollWeight, double yawWeight,
+                   bool initial_camera_constraint);
+
+  // Compute the weighted roll/yaw error between the current position and along-track
+  // direction. Recall that quaternion = cam2world = sat2World * rollPitchYaw * rotXY.
+  // rollPitchYaw is variable and can have jitter. Extract from it roll, pitch,
+  // yaw. See the .cc file for the implementation.
+  bool operator()(double const * const * parameters, double * residuals) const;
+
+  // Factory to hide the construction of the CostFunction object from
+  // the client code.
+  static ceres::CostFunction* Create(std::vector<double>           const& positions, 
+                                     std::vector<double>           const& quaternions, 
+                                     vw::cartography::GeoReference const& georef,
+                                     int cur_pos, 
+                                     double rollWeight, double yawWeight,
+                                     bool initial_camera_constraint) {
+
+    ceres::DynamicNumericDiffCostFunction<weightedRollYawError>* cost_function =
+          new ceres::DynamicNumericDiffCostFunction<weightedRollYawError>
+          (new weightedRollYawError(positions, quaternions, georef, cur_pos, 
+                                    rollWeight, yawWeight, initial_camera_constraint));
+
+    cost_function->SetNumResiduals(2); // for roll and yaw
+    cost_function->AddParameterBlock(NUM_QUAT_PARAMS);
+
+    return cost_function;
+  }
+
+  double m_rollWeight, m_yawWeight;
+  vw::Matrix3x3 m_rotXY, m_sat2World, m_initCam2World;
+  bool m_initial_camera_constraint;
+};
 
 } // end namespace asp
 
