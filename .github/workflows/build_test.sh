bc2dade927eaf006c67361dcfb5845348cdbcbd5
Oleg Alexandrov <oleg.alexandrov@gmail.com>
2023-09-25 14:11:17 -0700

    Add workflow for build and test on OSX

---
+++ .github/workflows/build_test.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo now in run_test.sh
+
+mkdir -p ~/work/StereoPipeline/StereoPipeline-OSX
+
+echo Created directory ~/work/StereoPipeline/StereoPipeline-OSX
+touch ~/work/StereoPipeline/StereoPipeline-OSX/test.txt
+echo Created file ~/work/StereoPipeline/StereoPipeline-OSX/test.txt
