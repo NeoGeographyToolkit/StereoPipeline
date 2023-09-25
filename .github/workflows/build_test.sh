6c7d2708f129839beacae8d9d5532f0dd626a54c
Oleg Alexandrov <oleg.alexandrov@gmail.com>
2023-09-25 14:57:25 -0700

    Flesh out the build_test.sh script

--- .github/workflows/build_test.sh
+++ .github/workflows/build_test.sh
@@ -1,10 +1,59 @@
 #!/bin/bash
 
-echo now in run_test.sh
+# Set up the compiler
+isMac=$(uname -s | grep Darwin)
+if [ "$isMac" != "" ]; then
+  cc_comp=clang
+  cxx_comp=clang++
+else
+  cc_comp=x86_64-conda_cos6-linux-gnu-gcc
+  cxx_comp=x86_64-conda_cos6-linux-gnu-g++
+fi
 
-dir=~/work/StereoPipeline/install/StereoPipeline-OSX
-mkdir -p $dir
+# Set up some variables
+aspRepoDir=$(pwd) # $HOME/work/StereoPipeline/StereoPipeline
+baseDir=$HOME/work/StereoPipeline
+installDir=$baseDir/install
+envPath=/usr/local/miniconda/envs/asp_deps
+# packageDir will later be uploaded, as set in the yml file
+packageDir=$baseDir/packages 
 
-echo Created file > $dir/test.txt
+# Build visionworkbench
+mkdir -p $baseDir
+cd $baseDir
+git clone https://github.com/visionworkbench/visionworkbench.git
+cd visionworkbench
+mkdir -p build
+cd build
+$envPath/bin/cmake ..                             \
+  -DASP_DEPS_DIR=$envPath                         \
+  -DCMAKE_VERBOSE_MAKEFILE=ON                     \
+  -DCMAKE_INSTALL_PREFIX=$installDir              \
+  -DCMAKE_C_COMPILER=${envPath}/bin/$cc_comp      \
+  -DCMAKE_CXX_COMPILER=${envPath}/bin/$cxx_comp
+make -j10 && make install
 
-echo content of file is $(cat $dir/test.txt)
+# Build StereoPipeline
+cd $aspRepoDir
+mkdir -p build
+cd build
+$envPath/bin/cmake ..                             \
+  -DASP_DEPS_DIR=$envPath                         \
+  -DCMAKE_VERBOSE_MAKEFILE=ON                     \
+  -DCMAKE_INSTALL_PREFIX=$installDir              \
+  -DVISIONWORKBENCH_INSTALL_DIR=$installDir       \
+  -DCMAKE_C_COMPILER=${envPath}/bin/$cc_comp      \
+  -DCMAKE_CXX_COMPILER=${envPath}/bin/$cxx_comp
+make -j10 && make install
+
+# Now package with BinaryBuilder
+cd $baseDir
+git clone https://github.com/NeoGeographyToolkit/BinaryBuilder
+cd BinaryBuilder
+./make-dist.py $installDir \
+  --asp-deps-dir $envPath  \
+  --python-env /usr/local/miniconda/envs/python_isis8
+
+# Prepare the package for upload
+mkdir -p $packageDir
+mv -fv Stereo* $packageDir
