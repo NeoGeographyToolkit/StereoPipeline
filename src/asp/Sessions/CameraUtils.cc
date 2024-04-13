88370c829dc1a089729f89bb43e882cabcdeb13d
Oleg Alexandrov <oleg.alexandrov@gmail.com>
2024-04-12 17:12:30 -0700

    Modularize camera loading

--- src/asp/Sessions/CameraUtils.cc
+++ src/asp/Sessions/CameraUtils.cc
@@ -30,11 +30,44 @@
 #include <string>
 #include <iostream>
 
-typedef boost::shared_ptr<asp::StereoSession> SessionPtr;
 
 using namespace vw;
 
 namespace asp {
+
+void load_camera(std::string const& image_file,
+                 std::string const& camera_file,
+                 std::string const& out_prefix,
+                 vw::GdalWriteOptions const& opt,
+                 bool approximate_pinhole_intrinsics,
+                 // Outputs
+                 std::string & stereo_session,
+                 vw::CamPtr  & camera_model,
+                 int         & single_threaded_camera) {
+
+  // TODO(oalexan1): Replace this with a simpler camera model loader class.
+  // But note that this call also refines the stereo session name.
+  asp::SessionPtr session
+    (asp::StereoSessionFactory::create(stereo_session, opt,
+                                        image_file, image_file,
+                                        camera_file, camera_file,
+                                        out_prefix));
+  
+  camera_model = session->camera_model(image_file, camera_file);
+  
+  // This is necessary to avoid a crash with ISIS cameras which is single-threaded
+  // TODO(oalexan1): Check this value for csm cameras embedded in ISIS images.
+  single_threaded_camera = (!session->supports_multi_threading());
+  
+  if (approximate_pinhole_intrinsics) {
+    boost::shared_ptr<vw::camera::PinholeModel> pinhole_ptr = 
+      boost::dynamic_pointer_cast<vw::camera::PinholeModel>(camera_model);
+    // Replace lens distortion with fast approximation
+    vw::camera::update_pinhole_for_fast_point2pixel<vw::camera::TsaiLensDistortion>
+      (*(pinhole_ptr.get()), file_image_size(image_file));
+  }
+
+}
 
 // Load cameras from given image and camera files
 void load_cameras(std::vector<std::string> const& image_files,
@@ -44,44 +77,38 @@ void load_cameras(std::vector<std::string> const& image_files,
                   bool approximate_pinhole_intrinsics,
                   // Outputs
                   std::string & stereo_session, // may change
-                  bool & single_threaded_cameras,
+                  bool & single_threaded_camera,
                   std::vector<boost::shared_ptr<vw::camera::CameraModel>> & camera_models) {
 
   // Initialize the outputs
   camera_models.clear();
-  single_threaded_cameras = false;
+  single_threaded_camera = false; // may change
   
   if (image_files.size() != camera_files.size()) 
     vw_throw(ArgumentErr() << "Expecting as many images as cameras.\n");  
+
+  // TODO(oalexan1): Must load one camera first. Then, if it is not single-threaded,
+  // load the rest in parallel. This can greatly speed up loading of many cameras.
+  // After the first camera is loaded, must populate the vector single_threaded_cameras_vec
+  // before continuing with the rest of the cameras.
+  
+  // Must initialize a vector of outputs, as we plan to load the cameras in parallel,
+  // and do not want to have thread-safety issues. Use a vector of ints, not bool,
+  // as compiling fails with a vector of bools.
+  std::vector<int> single_threaded_cameras_vec(image_files.size(), 
+                                               int(single_threaded_camera));
+  std::vector<std::string> stereo_sessions(image_files.size(), stereo_session);
+  camera_models.resize(image_files.size());
   
   for (size_t i = 0; i < image_files.size(); i++) {
-    vw_out(DebugMessage,"asp") << "Loading: " << image_files [i] << ' '
-                               << camera_files[i] << "\n";
-    
-    // The same camera is double-loaded into the same session instance.
-    // TODO: One day replace this with a simpler camera model loader class.
-    // But note that this call also refines the stereo session name.
-    SessionPtr session
-      (asp::StereoSessionFactory::create(stereo_session, opt,
-                                         image_files [i], image_files [i],
-                                         camera_files[i], camera_files[i],
-                                         out_prefix));
-    
-    camera_models.push_back(session->camera_model(image_files [i],
-                                                  camera_files[i]));
-    
-    // This is necessary to avoid a crash with ISIS cameras which is single-threaded
-    if (!session->supports_multi_threading())
-      single_threaded_cameras = true;
-    
-    if (approximate_pinhole_intrinsics) {
-      boost::shared_ptr<vw::camera::PinholeModel> pinhole_ptr = 
-        boost::dynamic_pointer_cast<vw::camera::PinholeModel>(camera_models.back());
-      // Replace lens distortion with fast approximation
-      vw::camera::update_pinhole_for_fast_point2pixel<vw::camera::TsaiLensDistortion>
-        (*(pinhole_ptr.get()), file_image_size(image_files[i]));
-    }
-  } // End loop through images loading all the camera models
+    load_camera(image_files[i], camera_files[i], out_prefix, opt,
+                approximate_pinhole_intrinsics,
+                stereo_sessions[i], camera_models[i], single_threaded_cameras_vec[i]);
+
+    // Update these based on the camera just loaded
+    single_threaded_camera = single_threaded_camera || single_threaded_cameras_vec[i];
+    stereo_session = stereo_sessions[i];
+  }
   
   return;
 }
@@ -106,11 +133,15 @@ bool datum_from_cameras(std::vector<std::string> const& image_files,
     if (stereo_session == "pinhole")
       stereo_session = "";
 
-    SessionPtr session(asp::StereoSessionFactory::create(stereo_session, // may change
-                                                         vw::GdalWriteOptions(),
-                                                         image_files [i], image_files [i],
-                                                         camera_files[i], camera_files[i],
-                                                         out_prefix)); 
+    std::string input_dem = ""; // No DEM
+    bool allow_map_promote = false, quiet = true;
+    asp::SessionPtr 
+      session(asp::StereoSessionFactory::create(stereo_session, // may change
+                                                vw::GdalWriteOptions(),
+                                                image_files [i], image_files [i],
+                                                camera_files[i], camera_files[i],
+                                                out_prefix, input_dem,
+                                                allow_map_promote, quiet));
     
     auto cam = session->camera_model(image_files[i], camera_files[i]);
     cam_center_radius = norm_2(cam->camera_center(vw::Vector2()));
@@ -149,7 +180,7 @@ bool datum_from_cameras(std::vector<std::string> const& image_files,
   }
   
   if (success)
-    vw_out() << "Guessed the datum from camera positions.\n";
+    vw_out() << "Guessed the datum from camera position.\n";
 
   return success;
 }
