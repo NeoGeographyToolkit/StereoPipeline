8950b2397f9b5951fe19e30b9a6147edac5f6300
Stefano Bertone <stefano.bertone@nasa.gov>
2026-09-24 23:47:24 -0400

    SfS: allow zero minimum brightness sigma

--- src/asp/SfS/SfsArgs.cc
+++ src/asp/SfS/SfsArgs.cc
@@ -448,9 +448,9 @@ void handleSfsArgs(int argc, char *argv[], SfsOptions& opt) {
     vw::vw_throw(vw::ArgumentErr()
                  << "--brightness-sigma-scaling must be non-negative.\n");
 
-  if (opt.brightness_sigma_scaling > 0.0 && opt.min_brightness_sigma <= 0.0)
+  if (opt.min_brightness_sigma < 0.0)
     vw::vw_throw(vw::ArgumentErr()
-                 << "--min-brightness-sigma must be positive.\n");
+                 << "--min-brightness-sigma must be non-negative.\n");
 
   if (opt.steepness_factor <= 0.0)
     vw::vw_throw(vw::ArgumentErr() << "The steepness factor must be positive.\n");
